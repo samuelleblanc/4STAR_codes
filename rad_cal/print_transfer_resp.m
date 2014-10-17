51e8208aa43dcd6712f5dbfb990289fcb7303c41
samuelleblanc <samuel.elie.leblanc@gmail.com>
2014-10-17 16:28:01 -0700

    incremental version update of print transfer

--- rad_cal/print_transfer_resp.m
+++ rad_cal/print_transfer_resp.m
@@ -27,7 +27,7 @@
 %  
 %
 % MODIFICATION HISTORY:
-% Written: Samuel LeBlanc, on Hercules C130, lat: 70 lon: -148, September 15th, 2014
+% Written (v1.0): Samuel LeBlanc, on Hercules C130, lat: 70 lon: -148, September 15th, 2014
 %
 % -------------------------------------------------------------------------
 
