6f724786715f074cacf214f5f2492c8d7d783619
samuelleblanc <samuel.elie.leblanc@gmail.com>
2014-11-12 12:01:14 -0800

    master change starc0

--- c0/starc0.m
+++ c0/starc0.m
@@ -11,6 +11,7 @@ function [visc0, nirc0, visnote, nirnote, vislstr, nirlstr, visaerosolcols, nira
 % Samuel, v1.0, 2014/10/13, added version_set, to version control the current m script
 % Samuel, v1.1, 2014/10/15, added verbose keyword
 % Yohei, 2014/11/12, test on Github
+% Sam test master
 version_set('1.1');
 if ~exist('verbose','var')
     verbose=true;
