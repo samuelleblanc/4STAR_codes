43a260b3cf17e08fa7e3df9ce3dbdc34c13a9901
samuelleblanc <samuel.elie.leblanc@gmail.com>
2014-11-12 13:43:57 -0800

    Joint editing test on starc0

--- c0/starc0.m
+++ c0/starc0.m
@@ -11,10 +11,14 @@ function [visc0, nirc0, visnote, nirnote, vislstr, nirlstr, visaerosolcols, nira
 % Samuel, v1.0, 2014/10/13, added version_set, to version control the current m script
 % Samuel, v1.1, 2014/10/15, added verbose keyword
 % Yohei, 2014/11/12, test on Github
+
+% testing of joint editing
 version_set('1.1');
 if ~exist('verbose','var')
     verbose=true;
 end;
+
+%Sam editing
 
 if verbose; disp('In starc0'), end;
 % control the input
