020c576248fbe129ab7db1cd872a2e826070ed56
samuelleblanc <samuel.elie.leblanc@gmail.com>
2014-11-12 13:58:43 -0800

    romved startup.m changed to startup_plotting.m

--- plotting_utils/startup_plotting.m
+++ plotting_utils/startup_plotting.m
@@ -1,6 +1,6 @@
 % program to put the default setup for making plots look good
 
-function startup
+function startup_plotting
  set(0,'defaultlinelinewidth',2)
  set(0,'defaultaxeslinewidth',2)
  set(0,'defaultpatchlinewidth',2)
