7ab47afe2ecb335ec325da2af29ad6e94fed20a9
segalm75 <segalm75@users.noreply.github.com>
2015-04-22 08:29:29 -0700

    revised o3 fit to accept linear baseline with log(lambda)

--- gas_phase/o3corecalc.m
+++ gas_phase/o3corecalc.m
@@ -2,9 +2,10 @@ function [O3conc H2Oconc O4conc O3resi o3OD] = o3corecalc(starsun,o3coef,o4coef,
 % retrieve o3 and derive fitted spectrum for subtraction
 % MS, 2015-04-18, changed function to retrieve using 2nd derivative 
 %                 instead of 3rd
-%-------------------------------------------------------
+% MS, 2015-04-20, changed retrieval to use first order poly
+%------------------------------------------------------------------
 % MS, July, 24, 2014
-%-------------------------------------------------------
+%------------------------------------------------------------------
 ODfit = zeros(length(starsun.t),length(starsun.w));
 tau_o3o4h2o_subtract = tau_OD;
 % subtract baseline
@@ -49,20 +50,24 @@ o3_DU = [];
 for i = 1:length(starsun.t)
            
     %x0 = [0.3 1 0.5 0.75 0.8 -2 -0.1]; % this is initial guess;o3-300h2o-5000;o4-10000
-    x0 = [0.3 1 0.5 0.75 0.8 -2]; % this is initial guess;o3-300h2o-5000;o4-10000
+    %x0 = [0.3 1 0.5 0.75 0.8 -2]; % 2nd order
+    x0 = [0.3 1 0.5 0.1 0]; % 1st order
+    
     y = (tau_OD(i,wln));
     meas = [starsun.w(wln)' y'];
     PAR  = [o3coef(wln) o4coef(wln)*10000 h2ocoef(wln)*10000];
        % Set Options
        
-       options = optimset('Algorithm','sqp','LargeScale','off','TolFun',1e-12,'Display','notify-detailed','TolX',1e-12,'MaxFunEvals',1000);%optimset('Algorithm','interior-point','TolFun',1e-12);%optimset('MaxIter', 400);
-       
+       %options = optimset('Algorithm','sqp','LargeScale','off','TolFun',1e-12,'Display','notify-detailed','TolX',1e-12,'MaxFunEvals',1000);%optimset('Algorithm','interior-point','TolFun',1e-12);%optimset('MaxIter', 400);
+       options = optimset('Algorithm','interior-point','LargeScale','off','TolFun',1e-6,'Display','notify-detailed','TolX',1e-6,'MaxFunEvals',1000);
        
        % bounds
 %            lb = [0 0 0 -10 -10 -10 -10];
 %            ub = [1 5 1 20 20 20 20];%o3-1000;h2o-10000o4-50000
-           lb = [0 0 0 -10 -10 -10];
-           ub = [1 5 1 20 20 20];%o3-1000;h2o-10000o4-50000
+%            lb = [0 0 0 -10 -10 -10];
+%            ub = [1 5 1 20 20 20];% 2nd order
+             lb = [0.20 0 0 0 -1];
+             ub = [0.45 5 0.5 1 0];% 1st order
            
  
        % check spectrum validity for conversion
@@ -93,42 +98,50 @@ for i = 1:length(starsun.t)
              
 %                    yopt_ =  exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*10000*real(U_(2)))).*exp(-(h2ocoef(wln).*10000*real(U_(3))))...
 %                             .*exp(-(U_(4) + U_(5)*meas(:,1) + U_(6)*meas(:,1).^2 + U_(7)*meas(:,1).^3));
-                   yopt_ =  exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*10000*real(U_(2)))).*exp(-(h2ocoef(wln).*10000*real(U_(3))))...
-                            .*exp(-(U_(4) + U_(5)*meas(:,1) + U_(6)*meas(:,1).^2));
-                   yopt  = -log(yopt_);
+%                    yopt_ =  exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*10000*real(U_(2)))).*exp(-(h2ocoef(wln).*10000*real(U_(3))))...
+%                             .*exp(-(U_(4) + U_(5)*meas(:,1) + U_(6)*meas(:,1).^2));
+%                    yopt_ =  exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*10000*real(U_(2)))).*exp(-(h2ocoef(wln).*10000*real(U_(3))))...
+%                             .*exp(-(U_(4) + U_(5)*meas(:,1)));%this was first order with error in meas dependent (lambda)
+                     yopt =  (o3coef(wln).*real(U_(1)) + (o4coef(wln).*10000*real(U_(2)))) + (h2ocoef(wln).*10000*real(U_(3)))...
+                              + U_(4)*ones(length(meas(:,1)),1) + U_(5)*log(meas(:,1));% this is with log(lambda)
+
+                   %yopt  = -log(yopt_);
                    yo3subtractall   =  -log(exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*10000*real(U_(2)))).*exp(-(h2ocoef(wln).*10000*real(U_(3)))));
                    yo3subtract   =  -log(exp(-(o3coef(wln).*real(U_(1)))));
                    yo4subtract   =  -log(exp(-(o4coef(wln).*10000*real(U_(2)))));
                    yh2osubtract  =  -log(exp(-(h2ocoef(wln).*10000*real(U_(3)))));
+                   %linesubtract  =  -log(exp(-(U_(4) + U_(5)*meas(:,1))));
+                   linesubtract  =  U_(4)*ones(length(meas(:,1)),1) + U_(5)*log(meas(:,1));
                
                % assign fitted spectrum
                 ODfit(i,wln) = yopt;
                 % save spectrum to subtract
                 tau_o3o4h2o_subtract(i,wln) = -log(exp(-(o3coef(wln).*real(U_(1)))).*exp(-(o4coef(wln).*real(U_(2)))).*exp(-(h2ocoef(wln).*real(U_(3)))));
-%                        figure(444);
-%                        plot(starsun.w(wln),y,'-b');hold on;
-%                        plot(starsun.w(wln),yopt,'--r');hold on;
-%                        %plot(starsun.w(wln),-log(Tmodel),'--r');hold off;
-%                        plot(starsun.w(wln),yo3subtract,'--g');hold on;
-%                        plot(starsun.w(wln),yo4subtract,'--c');hold on;
-%                        plot(starsun.w(wln),yh2osubtract,'--m');hold on;
-%                        plot(starsun.w(wln),y'-yo3subtractall,':k');hold off;
-%                        %plot(starsun.w(wln),tau_aero_subtract(i,wln),'-c');hold on;
-%                        %plot(starsun.w(wln),spectrum_sub(i,:),':c');hold on;
-%                        %plot(starsun.w(wln),y-tau_aero_subtract(i,wln),'-k');hold on;
-%                        %plot(starsun.w(wln),y-spectrum_sub(i,:),':k');hold on;
-%                        xlabel('wavelength','fontsize',12);ylabel('total OD','fontsize',12);
-%                        legend('measured','calculated (fit)','o3 spectrum to subtract','o4 spectrum to subtract','h2o spectrum to subtract','subtracted spectrum');
-%                        title([datestr(starsun.t(i),'yyyy-mm-dd HH:MM:SS') ' Alt= ' num2str(starsun.Alt(i)) 'm' ' O3= ' num2str(o3_DU_round) '[DU]']);
-%                        ymax = yopt + 0.2;
-%                        axis([min(starsun.w(wln)) max(starsun.w(wln)) 0 max(ymax)]);
-%                        pause(0.0001);
+                       figure(444);
+                       plot(starsun.w(wln),y,'-b');hold on;
+                       plot(starsun.w(wln),yopt,'--r');hold on;
+                       %plot(starsun.w(wln),-log(Tmodel),'--r');hold off;
+                       plot(starsun.w(wln),yo3subtract,'--g');hold on;
+                       plot(starsun.w(wln),yo4subtract,'--c');hold on;
+                       plot(starsun.w(wln),yh2osubtract,'--m');hold on;
+                       plot(starsun.w(wln),linesubtract,'-k');hold on;
+                       plot(starsun.w(wln),y'-yo3subtractall,':k');hold off;
+                       %plot(starsun.w(wln),tau_aero_subtract(i,wln),'-c');hold on;
+                       %plot(starsun.w(wln),spectrum_sub(i,:),':c');hold on;
+                       %plot(starsun.w(wln),y-tau_aero_subtract(i,wln),'-k');hold on;
+                       %plot(starsun.w(wln),y-spectrum_sub(i,:),':k');hold on;
+                       xlabel('wavelength','fontsize',12);ylabel('total OD','fontsize',12);
+                       legend('measured','calculated (fit)','o3 spectrum to subtract','o4 spectrum to subtract','h2o spectrum to subtract','aerosol baseline','subtracted spectrum');
+                       title([datestr(starsun.t(i),'yyyy-mm-dd HH:MM:SS') ' Alt= ' num2str(starsun.Alt(i)) 'm' ' O3= ' num2str(o3_DU_round) '[DU]']);
+                       ymax = yopt + 0.2;
+                       axis([min(starsun.w(wln)) max(starsun.w(wln)) 0 max(ymax)]);
+                       pause(0.0001);
                 % subtract fitted spectrum from slant
                 % tau_aero_subtract(i,wln) = tau_ODslant(i,wln) - ODfit(i,wln);
                 % hold on; plot(starsun.w(wln),yopt,'--g'); legend('measured','calculated (opt)','spectrum to subtract');
        else
           
-               U_ = [NaN NaN NaN NaN NaN NaN];
+               U_ = [NaN NaN NaN NaN NaN];
                sc = [sc; U_];
                sc_residual = [sc_residual;NaN];
                o3_DU = [o3_DU;NaN];
