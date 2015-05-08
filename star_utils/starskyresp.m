46a2eebd2c377f676e330c1bb847f662b3b40314
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-05-08 16:47:55 -0700

    Updates to radiance responses for ARISE

--- star_utils/starskyresp.m
+++ star_utils/starskyresp.m
@@ -87,13 +87,13 @@ if isnumeric(t); % time of the measurement is given; return the response of the 
         % with SEAC4RS fiber (not the long one going on ARISE)
         daystr = '20140624'; % date of cal
         filesuffix = 'from_20140624_016_VIS_park_with_20140606091700HISS';
-    elseif t >= datenum([2014 7 16 0 0 0]) && t < datenum([2014 10 25 0 0 0]);
+    elseif t >= datenum([2014 7 16 0 0 0]) && t < datenum([2014 9 13 0 0 0]);
         % for using calibration from second lab sphere cal
         % with long fiber for the ARISE field campaign
         daystr = '20140716';
         filesuffix = 'from_20140716_003_VIS_park_with_20140606091700HISS';
         %filesuffix = 'from_20140716_004_VIS_park_with_20140606091700HISS';
-    elseif t >= datenum([2014 10 25 0 0 0]);
+    elseif t >= datenum([2014 9 13 0 0 0]);
         % for using calibration from second lab sphere cal
         % with long fiber for the ARISE field campaign
         daystr = '20141024';
