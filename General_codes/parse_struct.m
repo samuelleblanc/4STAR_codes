89b1dbeb05908f30946d46c8170e46df3f2f885f
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-07-22 10:43:41 -0700

    applied toggle handling for input to starsun

--- General_codes/parse_struct.m
+++ General_codes/parse_struct.m
@@ -1,6 +1,7 @@
 %% Simple function to parse out the input varargin and take out a structure
 function [s,vout] = parse_struct(varargin)
 s = struct();
+vout = {};
 for i=1:nargin
     argi = varargin{i};
     if isstruct(argi);
