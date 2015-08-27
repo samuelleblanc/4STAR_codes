19c70300c514df88a3c8210a63bb5110607c5620
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-08-27 00:37:12 -0700

    New code for making figures saved with visible off to visible

---
+++ General_codes/plotting_utils/makevisible.m
@@ -0,0 +1,9 @@
+% Simple function to make a figure that was saved with visible off, to turn
+% it on
+
+function makevisible(file)
+f=load(file,'-mat');
+n=fieldnames(f);
+f.(n{1}).properties.Visible='on';
+save(file,'-struct','f')
+end
