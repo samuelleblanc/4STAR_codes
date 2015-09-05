388eca963a69dff7cfdff6e1d468fda010e35a65
samuelleblanc <samuel.elie.leblanc@gmail.com>
2015-09-04 17:08:19 -0700

    Modifications to starforj file to use starwrapper toggles.

--- 4STAR_codes/starforj.m
+++ 4STAR_codes/starforj.m
@@ -17,11 +17,33 @@ function [savematfile, contents]=starsun(varargin)
 %   [savematfile, contents]=starforj(...) returns the path for the resulting mat-file and its contents.
 % 
 % Yohei, 2013/02/07, after starsun.m.
+% Sam, v1.0,2015/09/01, added version control, change to use toggle switches as
+%                   input
+version_set('1.0');
 
 %********************
 % regulate input and read source
 %********************
-[sourcefile, contents0, savematfile]=startupbusiness('forj', varargin{:});
+
+toggle.verbose = true;
+toggle.applytempcorr = false;
+toggle.doflagging = false;
+toggle.gassubtract = false;
+toggle.runwatervapor = false;
+toggle.booleanflagging = false;
+toggle.flagging = 1; % for starflag, mode=1 for automatic, mode=2 for in-depth 'manual'
+toggle.doflagging = false; % for running any Yohei style flagging
+toggle.dostarflag = false; 
+
+if (~isempty(varargin));
+    [toggle_in,vars] = parse_struct(varargin{:});
+    toggle = catstruct(toggle,toggle_in);
+else
+    vars = varargin;
+end
+
+
+[sourcefile, contents0, savematfile]=startupbusiness('forj', vars{:});
 contents=[];
 if isempty(contents0);
     savematfile=[];
@@ -41,7 +63,7 @@ load(sourcefile,contents0{:});
 
 % add variables and make adjustments common among all data types. Also
 % combine the two structures.
-s=starwrapper(vis_forj, nir_forj);
+s=starwrapper(vis_forj, nir_forj,toggle);
 
 %********************
 % save
