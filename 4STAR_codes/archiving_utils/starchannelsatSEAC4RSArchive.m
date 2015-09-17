a0533046d12a5c85913f3e07aedf330dd600105e
yshinozu <Yohei.Shinozuka@nasa.gov>
2015-09-16 22:56:25 -0700

    Updated for the R2 processing

--- 4STAR_codes/archiving_utils/starchannelsatSEAC4RSArchive.m
+++ 4STAR_codes/archiving_utils/starchannelsatSEAC4RSArchive.m
@@ -2,17 +2,25 @@ function [visc,nirc,viscrange,nircrange]=starchannelsatSEAC4RSArchive(t)
 
 % returns the 4STAR visible channels that correspond to the SEAC4RS archive wavelengths.
 % Yohei, 2013/07/25, after starchannelsatTCAPArchive.m.
+% Yohei, 2015/09/16. Updated for the R2 processing.
 
 if nargin==1;
     [visw,nirw]=starwavelengths(t); 
 else
     [visw,nirw]=starwavelengths; 
 end;
-lambda=[0.355 0.3800    0.4518    0.5008    0.5200    0.5320    0.55  0.6055    0.6753  0.7806    0.8645  1.0199    1.0642    1.2358    1.5587];
-fwhm=zeros(size(lambda)); % PREDE FWHM !!! update this.
+if now>=datenum([2015 9 15 0 0 0]); % for R2 archive
+    lambda=[0.3800    0.4518    0.5008    0.5200    0.5320    0.55  0.6055 0.6197  0.6753  0.7806    0.8645  1.0199    1.0396 1.0642    1.2358    1.5587 1.6266];
+elseif now>=datenum([2014 4 1 0 0 0]); % for R0 archive
+    lambda=[0.3800    0.4518 0.5008    0.5200    0.5320    0.55  0.6055    0.6753  0.7806    0.8645  1.0199    1.0642    1.2358    1.5587];
+else
+    lambda=[0.355 0.3800    0.4518    0.5008    0.5200    0.5320    0.55  0.6055    0.6753  0.7806    0.8645  1.0199    1.0642    1.2358    1.5587];
+end;
+fwhm=zeros(size(lambda)); 
 visc=round(interp1(visw,1:length(visw),lambda));
 nirc=round(interp1(nirw,1:length(nirw),lambda));
 
+% The lines below are not currently meaningful; kept here for format consistency with starchannelsatAATS.m and for possible future use 
 viscrange(1,:)=ceil(interp1(visw, 1:length(visw), lambda-fwhm/2));
 viscrange(2,:)=floor(interp1(visw, 1:length(visw), lambda+fwhm/2));
 viscrange(2,find(isfinite(viscrange(2,:))==0&isfinite(viscrange(1,:))==1))=length(visw);
