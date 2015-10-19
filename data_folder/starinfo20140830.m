ed79063fd5374569789ee90868f51b01b98de606
segalm75 <segalm75@users.noreply.github.com>
2015-10-19 13:33:38 -0700

    starinfo ng update

---
+++ data_folder/starinfo20140830.m
@@ -0,0 +1,32 @@
+flight=[datenum('19:23:15') datenum('24:22:00')]-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]);% 
+% No good time periods ([start end]) and memo for all pixels
+%  flag: 1 for unknown or others, 2 for before and after measurements, 10 for unspecified type of clouds, 90 for cirrus, 100 for unspecified instrument trouble, 200 for instrument tests, 300 for frost.
+% daystr=mfilename;
+% daystr=daystr(end-7:end);
+% No good time periods ([start end]) for specific pixels
+% s.ng=s.ng;
+% STD-based cloud screening for direct Sun measurements
+s.sd_aero_crit=0.01;
+
+% Ozone and other gases
+s.O3h=21;
+s.O3col=0.295;%245+1std
+s.NO2col=3.91e15;
+
+% other tweaks
+if isfield(s, 'Pst');
+    s.Pst(find(s.Pst<10))=1013;
+end;
+if isfield(s, 'Lon') & isfield(s, 'Lat');
+    s.Lon(s.Lon==0 & s.Lat==0)=NaN;
+    s.Lat(s.Lon==0 & s.Lat==0)=NaN;
+end;
+if isfield(s, 'AZstep') & isfield(s, 'AZ_deg');
+    s.AZ_deg=s.AZstep/(-50);
+end;
+
+% notes
+if isfield(s, 'note');
+    s.note(end+1,1) = {['See ' mfilename '.m for additional info. ']};
+end;
+
