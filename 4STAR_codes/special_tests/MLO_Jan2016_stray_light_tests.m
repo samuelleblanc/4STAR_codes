31b642ffd70155b06fcdab879723be2f02728dab
ConnorJFlynn <D3K014@WE30976.pnl.gov>
2016-02-09 16:38:02 -0800

    MLO files
    auto-generated flag files and starting script for examining MLO stray
    light tests with Schott filters

---
+++ 4STAR_codes/special_tests/MLO_Jan2016_stray_light_tests.m
@@ -0,0 +1,20 @@
+2016-01-14 MLO file 007
+
+
+star_MLO_filters
+
+filter
+sp 1300     29      48
+sp kg5      90      110
+sp bg40     190     210
+sp bg38     245     261
+lp cg395    319     330
+lp cg455    358     375
+lp cg495    412     429
+lp og550    464     484
+lp og570    513     530
+lp rg610    564     587
+lp rg715    638     660
+lp rg790    692     717
+lp 1200     752     777
+lp 1350     822     849
