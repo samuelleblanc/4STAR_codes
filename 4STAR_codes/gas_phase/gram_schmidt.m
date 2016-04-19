b868c4ff60ea353547b2304746cb2a8406a90b6a
segalm75 <segalm75@users.noreply.github.com>
2016-04-18 21:55:20 -0700

    added orthoganalization routine

---
+++ 4STAR_codes/gas_phase/gram_schmidt.m
@@ -0,0 +1,16 @@
+% Here is a Matlab code for Gram-Schmidt
+% orthogonalization
+% =============================
+function [Q,R] = gram_schmidt(A)
+[m,n] = size(A);
+% compute QR using Gram-Schmidt
+for j = 1:n
+   v = A(:,j);
+   for i=1:j-1
+        R(i,j) = Q(:,i)'*A(:,j);
+        v = v - R(i,j)*Q(:,i);
+   end
+   R(j,j) = norm(v);
+   Q(:,j) = v/R(j,j);
+end
+% =============================
