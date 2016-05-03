507413b56394072c67334ff5ef3fbb6e988f7c32
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-05-03 13:59:36 +0900

    new starinfo files

---
+++ data_folder/starinfo20160501.m
@@ -0,0 +1,23 @@
+flight=[datenum(2016,5,1,23,57,0) datenum(2016,5,2,6,55,0)];
+
+% Ozone and other gases
+s.O3h=21; % Yohei's guess
+s.O3col=0.327; % Yohei's guess    
+s.NO2col=2.0e15; % Yohei's guess 
+
+% other tweaks
+if isfield(s, 'Pst');
+    s.Pst(find(s.Pst<10))=1013.25; 
+end;
+if isfield(s, 'Lon') & isfield(s, 'Lat');
+    s.Lon(s.Lon==0 & s.Lat==0)=NaN;
+    s.Lat(s.Lon==0 & s.Lat==0)=NaN;
+end;
+if isfield(s, 'AZstep') & isfield(s, 'AZ_deg');
+    s.AZ_deg=s.AZstep/(-50);
+end;
+
+% notes
+if isfield(s, 'note');
+    s.note(end+1,1) = {['See ' mfilename '.m for additional info. ']};
+end;
