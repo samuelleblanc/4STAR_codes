e97da6e7375b914516c4d4c46f225d19283e667a
segalm75 <segalm75@users.noreply.github.com>
2016-05-12 03:46:30 -0700

    updated o3 flags

--- data_folder/starinfo_20160426_1.m
+++ data_folder/starinfo_20160426_1.m
@@ -11,19 +11,17 @@ else
     s.toggle = update_toggle;
 end
 
-s.flight =[datenum(2016,5,1,22,30,00) datenum(2016,5,2,6,55,17)];  
-%s.ground =[datenum('02:37:27') datenum('06:07:07')] -datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]);
-    
- 
+s.flight =[datenum('12:33:54') datenum('17:15')] -datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]);   
+s.langley=[datenum('14:12:00') datenum('16:00')] -datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]);  
 % STD-based cloud screening for direct Sun measurements 
 s.sd_aero_crit=0.01; 
-s.flagfilename    = '20160501_starflag_man_created20160503_2330by_JR.mat';
-s.flagfilenameO3  = '20160501_starflag_O3_man_created20160510_0648by_MS.mat';
+s.flagfilename   = '20160426_starflag_man_created20160505_1153by_SL.mat';
+s.flagfilenameO3 = '20160426_1_starflag_O3_man_created20160511_2328by_MS.mat';
  
 % Ozone and other gases 
 s.O3h=21; 
-s.O3col=0.360;    % Michal's guess based on forecast 
-s.NO2col=3.0e16;  % Michal's guess; need to confirm. 
+s.O3col=0.320;    % OMI's mean values; first transit 
+s.NO2col=2.5e15;  % OMI's mean values; first transit 
  
 % other tweaks 
 if isfield(s, 'Pst'); 
@@ -32,10 +30,6 @@ end;
 if isfield(s, 'Lon') & isfield(s, 'Lat'); 
     s.Lon(s.Lon==0 & s.Lat==0)=NaN; 
     s.Lat(s.Lon==0 & s.Lat==0)=NaN; 
-    s.Lon(isnan(s.Lon))       =127.0264; % tweaks for Osan AFB
-    s.Lat(s.Lat==0)           =37.0878;  % tweaks for Osan AFB 
-    s.Alt(s.Alt==14)          =35.7;  % tweaks for Osan AFB
-    s.Alt(s.Alt==0)           =35.7;  % tweaks for Osan AFB 
 end; 
 if isfield(s, 'AZstep') & isfield(s, 'AZ_deg'); 
     s.AZ_deg=s.AZstep/(-50); 
