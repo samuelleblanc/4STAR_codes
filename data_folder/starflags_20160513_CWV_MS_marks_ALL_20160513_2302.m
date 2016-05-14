61379af1771cf422b3a18646e4c269e707f51ec6
segalm75 <segalm75@users.noreply.github.com>
2016-05-14 00:11:14 -0700

    updated flag routines and quickplots

---
+++ data_folder/starflags_20160513_CWV_MS_marks_ALL_20160513_2302.m
@@ -0,0 +1,10 @@
+function marks = starflags_20160513_CWV_MS_marks_ALL_20160513_2302  
+ % starflags file for 20160513 created by MS on 20160513_2302 to mark ALL conditions 
+ version_set('20160513_2302'); 
+ daystr = '20160513';  
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('05:19:44') datenum('05:19:52') 700 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
