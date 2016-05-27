4e56c06783e0435e9a82460d589fbf7722c53088
ConnorJFlynn <Connor.Flynn@pnnl.gov>
2016-05-28 08:08:49 +0900

    Speeding up star code
    Using combination of decrementing loops and matfile.io to speed up
    starwrapper and cwvcorecalc.

---
+++ data_folder/starinfo20160526.m
@@ -0,0 +1,25 @@
+flight=[datenum(2016,5,26,03,00,38) datenum(2016,5,26,07,10,32)]; !!! to be updated
+% spirals=[datenum(2016,4,21,21,45,50) datenum(2016,4,21,21,56,55)
+% datenum(2016,4,21,21,57,50) datenum(2016,4,21,22,12,00)];
+
+% Ozone and other gases
+s.O3h=21; % Yohei's guess
+s.O3col=0.300; % Yohei's guess    
+s.NO2col=2.0e15; % Yohei's guess 
+
+% other tweaks
+if isfield(s, 'Pst');
+    s.Pst(find(s.Pst<10))=1013.25; 
+end;
+if isfield(s, 'Lon') & isfield(s, 'Lat');
+    s.Lon(s.Lon==0 & s.Lat==0)=NaN;
+    s.Lat(s.Lon==0 & s.Lat==0)=NaN;
+end;
+if isfield(s, 'AZstep') & isfield(s, 'AZ_deg');
+    s.AZ_deg=s.AZstep/(-50);
+end;
+
+% notes
+if isfield(s, 'note');
+    s.note(end+1,1) = {['See ' mfilename '.m for additional info. ']};
+end;
