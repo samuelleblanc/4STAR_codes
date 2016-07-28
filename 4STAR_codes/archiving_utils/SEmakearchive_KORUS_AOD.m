e25dd354c3be7f2c52480321f9db8996e42f7584
segalm75 <segalm75@users.noreply.github.com>
2016-07-28 14:24:14 -0700

    modifications for the archive code

--- 4STAR_codes/archiving_utils/SEmakearchive_KORUS_AOD.m
+++ 4STAR_codes/archiving_utils/SEmakearchive_KORUS_AOD.m
@@ -124,7 +124,7 @@ form.qual_flag = '%1.0f';
 %% prepare list of details for each flight
 dslist={'20160426' '20160501' '20160503' '20160504' '20160506' '20160510' '20160511' '20160512' '20160516' '20160517' '20160519' '20160521' '20160524' '20160526' '20160529' '20160530' '20160601' '20160602' '20160604' '20160608' '20160609' '20160614' '20160617' '20160618'} ; %put one day string
 %Values of jproc: 1=archive 0=do not archive
-jproc=[         0          0          0          0          0          0          0          0          0          0          0           0         0          0          0          0          0          0          0          0          1          0          0          0] ; %set=1 to process
+jproc=[         0          0          0          0          0          0          0          0          0          0          0           0         0          0          0          0          0          0          0          0          0          1          0          0] ; %set=1 to process
 
 %% run through each flight, load and process
 idx_file_proc=find(jproc==1);
