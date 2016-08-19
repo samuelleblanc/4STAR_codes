75f7861d65e726f6a31f3461df87c311b8f6abe7
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-08-19 16:02:14 -0700

    some starinfo

---
+++ data_folder/starinfo_20160703.m
@@ -0,0 +1,86 @@
+function s = starinfo(s)
+if exist('s','var')&&isfield(s,'t')&&~isempty(s.t)
+   daystr = datestr(s.t(1),'yyyymmdd');
+else
+   daystr=evalin('caller','daystr');
+end
+
+if isfield(s, 'toggle')
+    s.toggle = update_toggle(s.toggle);
+else
+    s.toggle = update_toggle;
+end
+
+% flight=[datenum(2016,6,18,17,00,00) datenum(2016,6,18,22,34,43)]; 
+% spirals=[datenum(2016,4,21,21,45,50) datenum(2016,4,21,21,56,55) 
+% datenum(2016,4,21,21,57,50) datenum(2016,4,21,22,12,00)]; 
+ 
+% Ozone and other gases 
+s.O3h=21; % Yohei's guess 
+s.O3col=0.268; % Yohei's guess     
+s.NO2col=2.0e15; % Yohei's guess  
+ 
+% other tweaks 
+if isfield(s, 'Pst'); 
+    s.Pst(find(s.Pst<10))=680.25;  
+end; 
+if isfield(s, 'Lon') & isfield(s, 'Lat'); 
+    s.Lon(s.Lon==0 & s.Lat==0)=NaN; 
+    s.Lat(s.Lon==0 & s.Lat==0)=NaN; 
+end; 
+if isfield(s, 'AZstep') & isfield(s, 'AZ_deg'); 
+    s.AZ_deg=s.AZstep/(-50); 
+end; 
+ 
+% notes 
+if isfield(s, 'note'); 
+    s.note(end+1,1) = {['See ' mfilename '.m for additional info. ']}; 
+end; 
+
+%push variable to caller
+% Bad coding practice to blind-push variables to the caller.  
+% Creates potential for clobbering and makes collaborative coding more
+% difficult because fields appear in caller memory space undeclared.
+
+varNames=who();
+for i=1:length(varNames)
+    if ~strcmp(varNames{i},'s')
+        assignin('caller',varNames{i},eval(varNames{i}));
+    end
+end;
+
+return
+
+
+function toggle_out = update_toggle(toggle_in)
+% toggle_out = update_toggle(toggle_in)
+% Merge the optional "toggle_in" with user-supplied values in toggle_out
+% Frequently this instance will be shadowed by the internal function
+% of the same name defined beneath starinfo files.
+% un-test commit michal 2016-05-08
+
+toggle_out.subsetting_Tint = true;
+toggle_out.pca_filter = false;
+toggle_out.verbose=true;
+toggle_out.saveadditionalvariables=true;
+toggle_out.savefigure=false;
+toggle_out.computeerror=true;
+toggle_out.inspectresults=false;
+toggle_out.applynonlinearcorr=true;
+toggle_out.applytempcorr=false;% true is for SEAC4RS data
+toggle_out.gassubtract = false;
+toggle_out.booleanflagging = false;
+toggle_out.flagging = 1; % for starflag, mode=1 for automatic, mode=2 for in-depth 'manual'
+toggle_out.doflagging = false; % for running any Yohei style flagging
+toggle_out.dostarflag = false; 
+toggle_out.lampcalib  = false; 
+toggle_out.runwatervapor = false;
+toggle_out.applyforjcorr = true;
+toggle_out.applystraycorr = false;
+
+if exist('toggle_in', 'var')
+   toggle_out = catstruct(toggle_in, toggle_out);
+end
+
+return
+
