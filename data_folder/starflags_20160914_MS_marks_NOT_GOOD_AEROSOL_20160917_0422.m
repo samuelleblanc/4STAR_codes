1ac522f14ec38ab898b7c86546a08f9976c8d273
segalm75 <segalm75@users.noreply.github.com>
2016-09-17 04:37:59 -0700

    updated 20160914 starflags

---
+++ data_folder/starflags_20160914_MS_marks_NOT_GOOD_AEROSOL_20160917_0422.m
@@ -0,0 +1,43 @@
+function marks = starflags_20160914_MS_marks_NOT_GOOD_AEROSOL_20160917_0422  
+ % starflags file for 20160914 created by MS on 20160917_0422 to mark NOT_GOOD_AEROSOL conditions 
+ version_set('20160917_0422'); 
+ daystr = '20160914';  
+ % tag = 10: unspecified_clouds 
+ marks=[ 
+ datenum('09:25:47') datenum('09:25:47') 10 
+datenum('09:25:51') datenum('09:25:52') 10 
+datenum('10:00:18') datenum('10:00:20') 10 
+datenum('10:25:07') datenum('10:25:11') 10 
+datenum('10:26:38') datenum('10:26:44') 10 
+datenum('10:34:46') datenum('10:34:46') 10 
+datenum('10:41:18') datenum('10:41:21') 10 
+datenum('10:41:54') datenum('10:41:57') 10 
+datenum('10:43:41') datenum('10:43:52') 10 
+datenum('10:45:27') datenum('10:45:36') 10 
+datenum('11:10:49') datenum('11:10:53') 10 
+datenum('11:21:10') datenum('11:21:12') 10 
+datenum('11:21:49') datenum('11:24:19') 10 
+datenum('11:31:47') datenum('11:31:51') 10 
+datenum('11:32:06') datenum('11:32:06') 10 
+datenum('11:32:08') datenum('11:32:08') 10 
+datenum('11:32:36') datenum('11:34:39') 10 
+datenum('11:44:03') datenum('11:45:31') 10 
+datenum('12:05:24') datenum('12:05:25') 10 
+datenum('12:05:29') datenum('12:05:30') 10 
+datenum('12:05:52') datenum('12:05:53') 10 
+datenum('12:16:21') datenum('12:16:22') 10 
+datenum('12:17:31') datenum('12:17:34') 10 
+datenum('12:17:42') datenum('12:17:50') 10 
+datenum('12:20:18') datenum('12:30:58') 10 
+datenum('12:39:00') datenum('12:39:00') 10 
+datenum('12:40:01') datenum('12:40:04') 10 
+datenum('12:40:19') datenum('12:40:22') 10 
+datenum('12:43:30') datenum('13:04:34') 10 
+datenum('13:11:30') datenum('13:11:31') 10 
+datenum('14:11:16') datenum('14:11:16') 10 
+datenum('14:11:18') datenum('14:16:29') 10 
+datenum('15:30:09') datenum('15:30:13') 10 
+datenum('15:57:28') datenum('15:57:38') 10 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
