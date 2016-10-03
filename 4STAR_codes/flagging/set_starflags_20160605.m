05c48feb5be48d4418797616f7f3da8499972a7f
ConnorJFlynn <Connor.Flynn@pnnl.gov>
2016-10-03 07:49:05 -0700

    Fixed typo in max AOD threshold
    Fixed typo in max AOD threshold

--- 4STAR_codes/flagging/set_starflags_20160605.m
+++ 4STAR_codes/flagging/set_starflags_20160605.m
@@ -12,8 +12,13 @@ if ~isfield(flags, 'unspecified_clouds')
     flags.unspecified_clouds = false(size(inp.t));
 end
 flags.before_or_after_flight = inp.t<inp.flight(1) | inp.t>inp.flight(2);
-flags.unspecified_clouds = inp.aod_500nm>inp.aod_500nm_max | (inp.ang_noscreening<.2 & inp.aod_500nm>0.08) | inp.rawrelstd(:,1)>inp.sd_aero_crit;
-flags.bad_aod = inp.aod_500nm<0 | inp.aod_865nm<0 | ~isfinite(inp.aod_500nm) | ~isfinite(inp.aod_865nm) | ~(inp.Md==1) | ~(inp.Str==1) | (inp.m_aero>inp.m_aero_max) | (inp.c0_500nm<=0);
+flags.unspecified_clouds = inp.aod_500nm>inp.aod_500nm_max |...
+    (inp.ang_noscreening<.2 & inp.aod_500nm>inp.aod_500nm_max) | ...
+    inp.rawrelstd(:)>inp.sd_aero_crit;
+flags.bad_aod = inp.aod_500nm<inp.min_aod | inp.aod_865nm<inp.min_aod | ~isfinite(inp.aod_500nm) ...
+    | ~isfinite(inp.aod_865nm) | ~(inp.Md==1) | ~(inp.Str==1) | ...
+    (inp.m_aero>inp.m_aero_max) | (inp.c0_500nm<=0); 
+% c0 < 0 implies spectral region where we don't trust calibration
 if isfield(inp,'darkstd_500nm')
     flags.bad_aod = flags.bad_aod | (inp.raw_500nm-inp.dark_500nm)<=inp.darkstd_500nm;
 end
