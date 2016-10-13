c49de4738280a60188a8e1dce58844ed96c9724d
segalm75 <segalm75@users.noreply.github.com>
2016-10-13 16:43:35 -0700

    modifying gas codes

--- 4STAR_codes/gas_phase/retrieveNO2original.m
+++ 4STAR_codes/gas_phase/retrieveNO2original.m
@@ -40,9 +40,6 @@
 % MS, 2016-05-05, added usage of reference spectrum
 % MS, 2016-08-24, created a bug in reading c0gases and ref amount
 % MS, 2016-08-25, corrected a bug in reading Refspec
-% MS, 2016-10-12, added features from retrieveNO2wfit
-%                 determined polynomial order for ORACLES
-%                 determined refSpec for ORACLES: 07-02 from MLO
 % -------------------------------------------------------------------------
 %% function routine
 function [no2] = retrieveNO2(s,wstart,wend,mode)
@@ -58,86 +55,65 @@ loadCrossSections_global;
  
  wln = find(s.w<=s.w(iend)&s.w>=s.w(istart)); 
  
- % select NO2 absorbing band to plot residuals for
- ires   = interp1(s.w(wln),[1:length(s.w(wln))],0.470  ,'nearest');
+
+  % select NO2 absorbing band to plot residuals for
+ ires   = interp1(s.w(wln),[1:length(s.w(wln))],0.460  ,'nearest');
  
- [tmp]=starc0gases(nanmean(s.t),s.toggle.verbose,'NO2',mode);
+ % apply specified NO2 gas c0
+ % decide which c0 to use
+ % mode = 1;%0-MLO c0; 1-MLO ref spec
+  
+  [tmp]=starc0gases(nanmean(s.t),s.toggle.verbose,'NO2',mode);
   
   if mode==0
       % when mode==0 need to choose wln
-      c0 = tmp(wln)';
-      %c0  = repmat(c0_,length(s.t),1);
+      c0_ = tmp(wln)';
+      c0  = repmat(c0_,length(s.t),1);
   elseif mode==1
-      c0 = tmp.no2refspec;
-      %c0  = repmat(c0_,length(s.t),1);
+      c0_ = tmp.no2refspec;
+      c0  = repmat(c0_,length(s.t),1);
   end
   
  
  % calculate residual spectrum (Rayleigh subtracted)
-  eta = repmat(log(c0),length(s.t),1) - log(s.rateslant(:,wln)) - repmat(s.m_ray,1,length(wln)).*s.tau_ray(:,wln);
  
-  %ratesub=real(s.rate(:,wln)./(repmat(s.f,1,length(wln)))./tr(s.m_ray, s.tau_ray(:,wln)));
-  %eta    =real(-log(ratesub./repmat(c0,length(s.t),1)));
-  
+ if     s.t(1) <= datenum([2016 8 25 0 0 0]); 
+      % pre-ORACLES
+      
+        %eta = repmat(log(c0),length(s.t),1) - log(s.rateslant(:,wln)) - repmat(s.m_ray,1,length(wln)).*s.tau_ray(:,wln);
+        eta = log(c0) - log(s.rateslant(:,wln)) - repmat(s.m_ray,1,length(wln)).*s.tau_ray(:,wln);
+        
+        
+        %eta = log(c0./s.ratetot);% ratetot is Ray subtracted
+        
+ elseif s.t(1) > datenum([2016 8 25 0 0 0]);   
+      % ORACLES
+      % don't subtract Rayleigh
+      
+       %eta1 = repmat(log(c0),length(s.t),1) - log(s.rateslant(:,wln));
+       eta1 = log(c0) - log(s.rateslant(:,wln));
+       
+       %eta = -log(s.rateslant(:,wln)./(repmat((c0),length(s.t),1)));
+      
+ end
 
-%% fit a linear line to spectra
-%===============================
-% linear fit calculated and subtracted from the spectrum to get an estimation of the aerosol amount 
-% and any scattered contribution to the measurement
-% calculate linear fit and create residual spectrum
-
-for i=1:length(s.t)
-    p = polyfit(s.w(wln),eta(i,:),1);
-    %p1 = p(1);
-    %p0 = p(2);
-    f  = polyval(p,s.w(wln));
-    
-    % create residual eta spectrum
-    
-    eta_residual(i,:) = eta(i,:) - f;
-    
-    % plot
-    
-%     figure(2222);
-%     subplot(211);
-%     plot(s.w(wln),eta(i,:),'.-','markersize',8);hold on;
-%     plot(s.w(wln),f,'-r','linewidth',2);hold off;
-%     legend('total OD','OD linear fit');title(serial2Hh(s.t(i)));
-%     xlabel('wavelength');ylabel('OD');
-%     subplot(212);
-%     plot(s.w(wln),eta_residual(i,:),'-k','linewidth',2);
-%     xlabel('wavelength');ylabel('residual spectrum');
-%     pause(0.01);
-
-    
-end
-
-% this is to check residual dependance with SZA/airmass
 if plotting
-% plot residual to fit (which is 470nm).
+% plot residual to fit (ind 12 in wln, which is 459nm).
         figure;
-        plot(s.m_aero,1000*eta_residual(:,ires),'.','color',[0.8,0.8,0.8],'markersize',8);
+        plot(s.m_aero,eta(:,ires),'.','color',[0.8,0.8,0.8],'markersize',8);
         axis([min(s.m_aero) max(s.m_aero) -10 20]);
 end
-
-%% run retrieval
 
 % do linear retrieval first (no wavelength shift) 
 
 
     % this is end member array (original cross sections)
     
-    if     s.t(1) <= datenum([2016 8 25 0 0 0]); 
-      % pre-ORACLES
-      basis = [no2_298Kcoef(wln), -no2coefdiff(wln),o3coef(wln), o4coef(wln), ones(length(wln),1), s.w(wln)'.*ones(length(wln),1) ((s.w(wln)').^2).*ones(length(wln),1)];
+  
+    %basis = [no2_298Kcoef(wln), no2coefdiff(wln), o3coef(wln), o4coef(wln), ones(length(wln),1), s.w(wln)'.*ones(length(wln),1), ((s.w(wln)').^2).*ones(length(wln),1)];
+    basis = [no2_298Kcoef(wln), o3coef(wln), o4coef(wln), ones(length(wln),1), s.w(wln)'.*ones(length(wln),1), ((s.w(wln)').^2).*ones(length(wln),1) ((s.w(wln)').^3).*ones(length(wln),1)];
    
-    elseif s.t(1) > datenum([2016 8 25 0 0 0]);   
-      % ORACLES   
-      basis = [no2_298Kcoef(wln), -no2coefdiff(wln),o3coef(wln), o4coef(wln), ones(length(wln),1), s.w(wln)'.*ones(length(wln),1), ((s.w(wln)').^2).*ones(length(wln),1), ((s.w(wln)').^3).*ones(length(wln),1)];
-   
-    end
-    
-    
+
     % solve
     % x = real(Abasis\spectrum_sub');
     
@@ -165,7 +141,7 @@ end
    xts = 60/3600;   %60 sec in decimal time
    if mode==0
         % covert from atm x cm to molec/cm^2
-       no2_amount =  ccoef_d(1,:);% + ccoef_d(2,:);
+       no2_amount =  ccoef_d(1,:);
        no2VCD = real((((Loschmidt*no2_amount))./(s.m_NO2)')');
        tplot = serial2Hh(s.t); %tplot(tplot<10) = tplot(tplot<10)+24;
        [no2VCDsmooth, sn] = boxxfilt(tplot, no2VCD, xts);
@@ -174,7 +150,6 @@ end
        % load reference spectrum
        % ref_spec = load([starpaths,'20160113NO2refspec.mat']);
        no2SCD = real((((Loschmidt*ccoef_d(1,:))))') + tmp.no2scdref;%ref_spec.no2col*ref_spec.mean_m;
-       %no2SCD = real(ccoef_d(1,:) + ccoef_d(2,:))*Loschmidt + tmp.no2scdref;%ref_spec.no2col*ref_spec.mean_m;
        tplot = serial2Hh(s.t); %tplot(tplot<10) = tplot(tplot<10)+24;
        [no2SCDsmooth, sn] = boxxfilt(tplot, no2SCD, xts);
        no2vcd_smooth = real(no2SCDsmooth)./s.m_NO2;
@@ -188,16 +163,10 @@ end
    RMSEno2  = real(sqrt(real(MSEno2DU)))./s.m_NO2; % convert to vertical
           
    % prepare to plot spectrum OD and no2 cross section
-   %no2spectrum     = eta-RR_d' + (ccoef_d(1,:))'*basis(:,1)';
-   % this is to account for temp of xs
-   no2spectrum     = eta-RR_d' + ((ccoef_d(1,:) + ccoef_d(2,:)))'*(basis(:,1)+basis(:,2))';%((ccoef_d(1,:) + ccoef_d(2,:)))'*basis(:,1)';
-   % this is not temp diff.
-   %no2fit          = (ccoef_d(1,:))'*basis(:,1)';
-   no2fit          = (ccoef_d(1,:) + ccoef_d(2,:))'*(basis(:,1)+basis(:,2))';
-   no2residual     = real(eta-RR_d');
-   t=nansum((no2residual).^2,2);
-   RMSres = sqrt(t)/(length(wln)-size(basis,2));
-   %RMSres = real(sqrt((nansum((no2residual).^2,2))/(length(wln)-size(basis,2))));
+   no2spectrum     = eta-RR_d' + (ccoef_d(1,:))'*basis(:,1)';
+   no2fit          = (ccoef_d(1,:))'*basis(:,1)';
+   no2residual     = eta-RR_d';
+   RMSres = real(sqrt((sum((no2residual).^2,2))/(length(wln)-7)));
 %    no2meas     = spectrum_sub - exp-((ccoef_d(2,:)/no2diff_norm')*basis(:,2)' - (ccoef_d(3,:)/o3_norm')*basis(:,3)' - (ccoef_d(4,:)')*basis(:,4)' - ccoef_d(5,:)'*basis(:,5)');
 %    no2fit      = exp(-(ccoef_d(1,:)')*basis(:,1)');
 %    no2residual = spectrum_sub - exp(-(ccoef_d(1,:)')*basis(:,1)' - (ccoef_d(2,:)')*basis(:,2)' - (ccoef_d(3,:)')*basis(:,3)' - ccoef_d(4,:)'*basis(:,4)' - ccoef_d(5,:)'*basis(:,5)');
@@ -207,7 +176,7 @@ end
    % plot fitted and "measured" no2 spectrum
          for i=1:500:length(s.t)
              figure(8882);
-             %plot(s.w((wln)),eta1(i,:),'-y','linewidth',2);hold on;
+             plot(s.w((wln)),eta1(i,:),'-y','linewidth',2);hold on;
              plot(s.w((wln)),eta(i,:),'--k','linewidth',2);hold on;
              plot(s.w((wln)),s.tau_aero(i,wln).*s.m_aero(i),'--m','linewidth',2);hold on;
              plot(s.w((wln)),RR_d(:,i),'--c','linewidth',2);hold on;
@@ -218,7 +187,7 @@ end
                     'fontsize',14,'fontweight','bold');
              ylabel('OD','fontsize',14,'fontweight','bold');
              %legend('measured NO_{2} spectrum','fitted NO_{2} spectrum','residual');
-             legend('total spectrum baseline and rayliegh subtracted','tau-aero','reconstructed spectrum','measured NO_{2} spectrum','fitted NO_{2} spectrum','residual');
+             legend('total spectrum not subtracted','total spectrum baseline and rayliegh subtracted','tau-aero','reconstructed spectrum','measured NO_{2} spectrum','fitted NO_{2} spectrum','residual');
              set(gca,'fontsize',12,'fontweight','bold');%axis([wstart wend -5e-3 0.04]);%legend('boxoff');
              pause;
          end
@@ -227,20 +196,15 @@ end
    
    if plotting
        figure;subplot(211);%plot(tplot,no2VCD,'.r');hold on;
-              %plot(tplot,no2vcd_smooth,'.g');hold on;
-              plot(tplot,no2vcd_smooth/(Loschmidt/1000),'.g');hold on;
-              %axis([tplot(1) tplot(end) 0 5e17]);
-              axis([tplot(1) tplot(end) 0 10]);
-              %xlabel('time');ylabel('no2 [molec/cm^{2}]');title(datestr(s.t(1),'yyyymmdd'));
-              xlabel('time');ylabel('no2 [DU]');title(datestr(s.t(1),'yyyymmdd'));
+              plot(tplot,no2vcd_smooth,'.g');hold on;
+              axis([tplot(1) tplot(end) 0 5e15]);
+              xlabel('time');ylabel('no2 [molec/cm^{2}]');title(datestr(s.t(1),'yyyymmdd'));
               %legend('linear inversion','linear inversion smooth');
               legend('linear inversion smooth');
               subplot(212);plot(tplot,RMSres,'.r');hold on;
               axis([tplot(1) tplot(end) 0 0.005]);
               xlabel('time');ylabel('no2 RMSE');
    end
-   
-   %% save variables
 
    % no2OD is the spectrum portion to subtract
     no2.no2_molec_cm2    = no2vcd_smooth;
@@ -248,6 +212,6 @@ end
     no2.no2OD            = (real((((Loschmidt*ccoef_d(1,:))))')*no2_298Kcoef')./repmat(s.m_NO2,1,length(s.w));%(no2VCD/Loschmidt)*no2_298Kcoef';% this is optical depth
     no2.no2SCD           = no2SCDsmooth;%real((((Loschmidt*ccoef_d(1,:))))');%no2SCDsmooth;%real((((Loschmidt*ccoef_d(1,:))))');
 
-   
+
 
 return;
