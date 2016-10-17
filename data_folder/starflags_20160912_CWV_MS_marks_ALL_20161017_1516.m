8994089ae95c0c5508208ccfde71f874cc28d5de
segalm75 <segalm75@users.noreply.github.com>
2016-10-17 15:20:23 -0700

    updating gases flags and starinfo for 0912

---
+++ data_folder/starflags_20160912_CWV_MS_marks_ALL_20161017_1516.m
@@ -0,0 +1,72 @@
+function marks = starflags_20160912_CWV_MS_marks_ALL_20161017_1516  
+ % starflags file for 20160912 created by MS on 20161017_1516 to mark ALL conditions 
+ version_set('20161017_1516'); 
+ daystr = '20160912';  
+ % tag = 2: before_or_after_flight 
+ % tag = 3: t 
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('07:32:49') datenum('15:59:26') 03 
+datenum('15:58:09') datenum('15:59:26') 02 
+datenum('07:32:49') datenum('07:32:59') 700 
+datenum('07:51:14') datenum('07:51:19') 700 
+datenum('08:09:34') datenum('08:09:39') 700 
+datenum('08:27:54') datenum('08:27:59') 700 
+datenum('08:46:14') datenum('08:46:19') 700 
+datenum('09:04:34') datenum('09:04:39') 700 
+datenum('09:22:55') datenum('09:22:59') 700 
+datenum('09:41:15') datenum('09:41:19') 700 
+datenum('09:59:35') datenum('09:59:40') 700 
+datenum('10:17:55') datenum('10:18:00') 700 
+datenum('10:36:15') datenum('10:36:20') 700 
+datenum('10:54:35') datenum('10:54:40') 700 
+datenum('11:12:56') datenum('11:13:00') 700 
+datenum('11:30:15') datenum('11:45:33') 700 
+datenum('11:57:03') datenum('11:57:03') 700 
+datenum('11:57:08') datenum('12:05:28') 700 
+datenum('12:07:27') datenum('12:07:31') 700 
+datenum('12:09:25') datenum('12:13:38') 700 
+datenum('12:39:39') datenum('12:39:44') 700 
+datenum('13:00:55') datenum('13:01:00') 700 
+datenum('13:22:17') datenum('13:22:17') 700 
+datenum('13:22:35') datenum('13:22:40') 700 
+datenum('13:23:03') datenum('13:23:03') 700 
+datenum('13:23:11') datenum('13:23:11') 700 
+datenum('13:23:21') datenum('13:23:21') 700 
+datenum('13:27:48') datenum('13:27:48') 700 
+datenum('13:28:15') datenum('13:28:16') 700 
+datenum('13:28:24') datenum('13:28:24') 700 
+datenum('13:28:46') datenum('13:28:48') 700 
+datenum('13:28:56') datenum('13:28:57') 700 
+datenum('13:30:38') datenum('13:30:39') 700 
+datenum('13:30:48') datenum('13:30:50') 700 
+datenum('13:31:04') datenum('13:31:04') 700 
+datenum('13:33:18') datenum('13:33:22') 700 
+datenum('13:33:27') datenum('13:33:29') 700 
+datenum('13:35:23') datenum('13:35:24') 700 
+datenum('13:35:30') datenum('13:35:31') 700 
+datenum('13:35:49') datenum('13:35:49') 700 
+datenum('13:36:05') datenum('13:36:06') 700 
+datenum('13:36:11') datenum('13:36:11') 700 
+datenum('13:36:24') datenum('13:36:26') 700 
+datenum('13:37:07') datenum('13:37:07') 700 
+datenum('13:37:41') datenum('13:37:41') 700 
+datenum('13:38:10') datenum('13:38:11') 700 
+datenum('13:38:15') datenum('13:38:15') 700 
+datenum('13:39:12') datenum('13:39:12') 700 
+datenum('13:39:23') datenum('13:39:24') 700 
+datenum('13:39:54') datenum('13:39:54') 700 
+datenum('13:41:37') datenum('13:41:37') 700 
+datenum('13:42:02') datenum('13:42:02') 700 
+datenum('13:48:08') datenum('13:48:13') 700 
+datenum('14:08:51') datenum('14:08:56') 700 
+datenum('14:27:11') datenum('14:27:16') 700 
+datenum('14:45:31') datenum('14:45:36') 700 
+datenum('15:03:52') datenum('15:03:56') 700 
+datenum('15:22:12') datenum('15:22:16') 700 
+datenum('15:25:03') datenum('15:25:03') 700 
+datenum('15:40:32') datenum('15:40:36') 700 
+datenum('15:41:14') datenum('15:59:26') 700 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
