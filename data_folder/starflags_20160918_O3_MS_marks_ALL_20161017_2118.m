48f3093f59879cce6fdb63f0a13b24825a697578
segalm75 <segalm75@users.noreply.github.com>
2016-10-17 21:27:13 -0700

    gas flagging and updating starinfo 0914 and 0918

---
+++ data_folder/starflags_20160918_O3_MS_marks_ALL_20161017_2118.m
@@ -0,0 +1,82 @@
+function marks = starflags_20160918_O3_MS_marks_ALL_20161017_2118  
+ % starflags file for 20160918 created by MS on 20161017_2118 to mark ALL conditions 
+ version_set('20161017_2118'); 
+ daystr = '20160918';  
+ % tag = 3: t 
+ % tag = 10: unspecified_clouds 
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('05:55:25') datenum('15:15:28') 03 
+datenum('05:55:25') datenum('05:55:34') 700 
+datenum('06:00:25') datenum('06:00:27') 700 
+datenum('06:00:29') datenum('06:03:37') 700 
+datenum('06:21:53') datenum('06:21:57') 700 
+datenum('06:40:13') datenum('06:40:17') 700 
+datenum('06:55:02') datenum('07:05:18') 700 
+datenum('07:20:31') datenum('07:20:35') 700 
+datenum('07:38:51') datenum('07:38:55') 700 
+datenum('07:57:11') datenum('07:57:16') 700 
+datenum('08:15:31') datenum('08:15:36') 700 
+datenum('08:33:51') datenum('08:33:56') 700 
+datenum('08:52:11') datenum('08:52:16') 700 
+datenum('09:10:33') datenum('09:10:37') 700 
+datenum('09:28:53') datenum('09:28:57') 700 
+datenum('09:47:03') datenum('09:48:26') 700 
+datenum('09:48:43') datenum('09:48:44') 700 
+datenum('09:48:49') datenum('09:49:03') 700 
+datenum('09:58:54') datenum('09:59:26') 700 
+datenum('09:59:30') datenum('09:59:59') 700 
+datenum('10:41:51') datenum('10:43:01') 700 
+datenum('10:43:06') datenum('10:43:09') 700 
+datenum('10:43:36') datenum('10:43:39') 700 
+datenum('10:46:50') datenum('10:47:00') 700 
+datenum('10:48:16') datenum('10:48:20') 700 
+datenum('10:58:42') datenum('10:58:42') 700 
+datenum('10:59:28') datenum('10:59:30') 700 
+datenum('11:06:36') datenum('11:06:41') 700 
+datenum('11:09:36') datenum('11:09:37') 700 
+datenum('11:19:13') datenum('11:19:13') 700 
+datenum('11:21:46') datenum('11:21:47') 700 
+datenum('11:22:28') datenum('11:22:32') 700 
+datenum('11:23:03') datenum('11:23:42') 700 
+datenum('11:24:17') datenum('11:24:18') 700 
+datenum('11:27:42') datenum('11:27:53') 700 
+datenum('11:27:57') datenum('11:28:01') 700 
+datenum('11:28:18') datenum('11:28:55') 700 
+datenum('11:32:21') datenum('11:32:49') 700 
+datenum('11:33:01') datenum('11:33:29') 700 
+datenum('11:34:12') datenum('11:34:36') 700 
+datenum('11:34:46') datenum('11:34:46') 700 
+datenum('11:38:17') datenum('11:38:25') 700 
+datenum('11:41:01') datenum('11:41:09') 700 
+datenum('11:47:12') datenum('11:47:16') 700 
+datenum('11:54:18') datenum('11:56:50') 700 
+datenum('12:06:49') datenum('12:06:53') 700 
+datenum('12:10:29') datenum('12:11:40') 700 
+datenum('12:11:44') datenum('12:12:12') 700 
+datenum('12:13:05') datenum('12:13:32') 700 
+datenum('12:13:38') datenum('12:14:05') 700 
+datenum('12:25:09') datenum('12:25:13') 700 
+datenum('12:48:16') datenum('12:48:21') 700 
+datenum('13:06:36') datenum('13:06:41') 700 
+datenum('13:24:56') datenum('13:25:01') 700 
+datenum('13:43:16') datenum('13:43:21') 700 
+datenum('14:01:37') datenum('14:01:41') 700 
+datenum('14:19:57') datenum('14:20:01') 700 
+datenum('14:38:17') datenum('14:38:21') 700 
+datenum('14:56:37') datenum('14:56:41') 700 
+datenum('15:03:48') datenum('15:15:28') 700 
+datenum('10:46:22') datenum('10:47:29') 10 
+datenum('11:21:58') datenum('11:24:12') 10 
+datenum('11:24:47') datenum('11:24:47') 10 
+datenum('11:27:13') datenum('11:29:24') 10 
+datenum('11:32:21') datenum('11:34:36') 10 
+datenum('11:37:47') datenum('11:38:54') 10 
+datenum('11:40:31') datenum('11:41:32') 10 
+datenum('11:41:34') datenum('11:41:35') 10 
+datenum('11:53:47') datenum('11:53:50') 10 
+datenum('11:53:53') datenum('11:54:28') 10 
+datenum('11:56:41') datenum('11:57:21') 10 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
--- data_folder/starinfo_20160914.m
+++ data_folder/starinfo_20160914.m
@@ -24,9 +24,10 @@ s.NO2col=2.0e15; % %
 
 % flags
 s.flagfilename = '20160914_starflag_man_created20160917_0422by_MS.mat'; 
-%s.flagfilenameO3 = '20160530_starflag_O3_man_created20160701_1630by_MS.mat';
-%s.flagfilenameCWV = '20160530_starflag_CWV_man_created20160701_1609by_MS.mat';
-
+s.flagfilenameCWV  = '20160914_starflag_CWV_man_created20161017_1550by_MS.mat';
+s.flagfilenameO3   = '20160914_starflag_O3_man_created20161017_1551by_MS.mat';
+s.flagfilenameNO2  = '20160914_starflag_NO2_man_created20161017_2109by_MS.mat';
+s.flagfilenameHCOH = '20160914_starflag_HCOH_man_created20161017_2113by_MS.mat';
  
 % other tweaks 
 if isfield(s, 'Pst'); 
