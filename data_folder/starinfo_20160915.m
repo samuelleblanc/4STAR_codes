d48d111e19be5aa17dc069249e0c492b2f81f184
samuelleblanc <samuel.elie.leblanc@gmail.com>
2016-10-19 15:56:41 -0700

    updated dirty clean periods in starinfo

--- data_folder/starinfo_20160915.m
+++ data_folder/starinfo_20160915.m
@@ -11,22 +11,25 @@ else
     s.toggle = update_toggle;
 end
 
-s.flight=[datenum(2016,9,8,7,05,08) datenum(2016,9,8,15,01,20)]; 
+ 
+s.flight=[datenum(2016,9,14,07,49,00) datenum(2016,9,14,15,54,54)]; % estimated
 % spirals=[datenum(2016,4,21,21,45,50) datenum(2016,4,21,21,56,55) 
 % datenum(2016,4,21,21,57,50) datenum(2016,4,21,22,12,00)]; 
-%s.langley=[datenum(2016,8,25,17,00,00) datenum(2016,8,25,19,15,00)];
-s.dirty = [datenum(2016,9,09,08,37,00) datenum(2016,9,09,08,39,00)];
-s.clean = [datenum(2016,9,09,13,59,00) datenum(2016,9,09,14,02,00)];
-
-s.flagfilenameCWV  = '20160908_starflag_CWV_man_created20161017_1454by_MS.mat';
-s.flagfilenameO3   = '20160908_starflag_O3_man_created20161017_1455by_MS.mat';
-s.flagfilenameNO2  = '20160908_starflag_NO2_man_created20161017_1457by_MS.mat';
-s.flagfilenameHCOH = '20160906_starflag_HCOH_man_created20161017_1446by_MS.mat';
+% s.langley=[datenum(2016,8,25,17,00,00) datenum(2016,8,25,19,15,00)];
+s.dirty = [datenum(2016,9,15,09,23,00) datenum(2016,9,15,09,24,00)];
+s.clean = [datenum(2016,9,15,09,41,00) datenum(2016,9,15,09,42,00)];
  
 % Ozone and other gases 
 s.O3h=21; % 
-s.O3col=0.280; %      
-s.NO2col=2.0e15; %  
+s.O3col=0.300; % Michal's guess     
+s.NO2col=2.0e15; % % 
+
+% flags
+s.flagfilename = '20160914_starflag_man_created20160917_0422by_MS.mat'; 
+s.flagfilenameCWV  = '20160914_starflag_CWV_man_created20161017_1550by_MS.mat';
+s.flagfilenameO3   = '20160914_starflag_O3_man_created20161017_1551by_MS.mat';
+s.flagfilenameNO2  = '20160914_starflag_NO2_man_created20161017_2109by_MS.mat';
+s.flagfilenameHCOH = '20160914_starflag_HCOH_man_created20161017_2113by_MS.mat';
  
 % other tweaks 
 if isfield(s, 'Pst'); 
