ae1cc72d2d47ba6601c1430d85bf56a5fcfc526f
segalm75 <segalm75@users.noreply.github.com>
2017-01-10 04:33:44 -0800

    updating 20160608 gas flags and starinfo

---
+++ data_folder/starflags_20160608_HCOH_MS_marks_NOT_GOOD_AEROSOL_20170110_0432.m
@@ -0,0 +1,13 @@
+function marks = starflags_20160608_HCOH_MS_marks_NOT_GOOD_AEROSOL_20170110_0432  
+ % starflags file for 20160608 created by MS on 20170110_0432 to mark NOT_GOOD_AEROSOL conditions 
+ version_set('20170110_0432'); 
+ daystr = '20160608';  
+ % tag = 3: t 
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('21:55:53') datenum('07:32:23') 03 
+datenum('21:55:53') datenum('03:14:55') 700 
+datenum('27:14:58') datenum('07:32:23') 700 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
