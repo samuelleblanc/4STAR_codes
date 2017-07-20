bbb8831a06b2f226dcde205a13a851f0e3295927
segalm75 <segalm75@users.noreply.github.com>
2017-07-20 16:40:00 -0700

    testing starflag

---
+++ data_folder/starflags_20160503_MS_marks_NOT_GOOD_AEROSOL_20170720_1437.m
@@ -0,0 +1,10 @@
+function marks = starflags_20160503_MS_marks_NOT_GOOD_AEROSOL_20170720_1437  
+ % starflags file for 20160503 created by MS on 20170720_1437 to mark NOT_GOOD_AEROSOL conditions 
+ version_set('20170720_1437'); 
+ daystr = '20160503';  
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('22:52:14') datenum('23:06:06') 700 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
