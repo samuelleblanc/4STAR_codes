8f53dfd510224b97b2e1be368c51945926acbc77
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2018-02-20 15:00:25 -0800

    MLO updates with last c0 and comparison, and starinfo

--- 4STAR_codes/special_tests/compare_MLO_Feb2018.m
+++ 4STAR_codes/special_tests/compare_MLO_Feb2018.m
@@ -3,7 +3,7 @@
 clear all
 fp = starpaths; %'C:\Users\sleblan2\Research\4STAR_codes\data_folder\';
 if ~isunix;
-fp_out = 'C:\Users\sleblanc\Research\ORACLES\plot\';
+fp_out = 'C:\Users\sleblanc\Research\MLO\2018_Febuary\';
 else;
 fp_out = '/nobackup/sleblan2/ORACLES/plot/';
 end;
@@ -11,6 +11,7 @@ asktosave = 1; %set if ask to save the figures
 i_avg = [1,2,3,4];
 filesuffix = ['4STAR_refined_averaged_MLO_Feb2018'];
 label_daystr = '20180212';
+fignames = 'MLO_Feb2018';
 
 vis_names = {'20180209_VIS_C0_refined_langley_4STARam_MLOFeb2018_day5_.dat';...
             '20180210_VIS_C0_refined_langley_4STARam_MLOFeb2018_day6.dat';...
@@ -18,6 +19,7 @@ vis_names = {'20180209_VIS_C0_refined_langley_4STARam_MLOFeb2018_day5_.dat';...
             '20180212_VIS_C0_refined_langley_4STARam_MLOFeb2018_day8.dat';...
             '20180207_VIS_C0_refined_langley_4STARam_MLOFeb2018_day3_.dat';...
             '20180208_VIS_C0_refined_langley_4STARam_MLOFeb2018_day4_.dat';...
+            '20180215_VIS_C0_refined_langley_4STARam_MLOFeb2018_day11.dat';...
             '20170815_VIS_C0_refined_averaged_MLO_inflightsubset_polyfit.dat';...
             '20171206_VIS_C0_refined_averaged_ames_roof_and_ORACLES2017.dat'};
         
@@ -27,11 +29,12 @@ nir_names = {'20180209_NIR_C0_refined_langley_4STARam_MLOFeb2018_day5_.dat';...
             '20180212_NIR_C0_refined_langley_4STARam_MLOFeb2018_day8.dat';...
             '20180207_NIR_C0_refined_langley_4STARam_MLOFeb2018_day3_.dat';...
             '20180208_NIR_C0_refined_langley_4STARam_MLOFeb2018_day4_.dat';...
+            '20180215_NIR_C0_refined_langley_4STARam_MLOFeb2018_day11.dat';...
             '20170815_NIR_C0_refined_averaged_MLO_inflightsubset_polyfit.dat';...
             '20171206_NIR_C0_refined_averaged_ames_roof_and_ORACLES2017.dat'};
          
 supp = {'MLO good';'MLO good';'MLO good';'MLO good';...
-        'MLO bad';'MLO marginal';...
+        'MLO bad';'MLO marginal';'MLO marginal';...
         'ORACLES avg';'Ames roof average'};
     
 n = length(vis_names);
@@ -140,19 +143,19 @@ leg{i+1} = ['Averages with ' gdaystr];
 legend(p,leg,'location','northeastoutside');
 
 % Save the figures
-save_fig(fig,[fp_out 'ORACLES2017_ames_roof_cal_c0_' gdaystr],asktosave);
-save_fig(fig2,[fp_out 'ORACLES2017_ames_roof_cal_c0_relative_' gdaystr],asktosave);
+save_fig(fig,[fp_out fignames '_cal_c0_' gdaystr],asktosave);
+save_fig(fig2,[fp_out fignames '_cal_c0_relative_' gdaystr],asktosave);
 
 figure(fig2);
 ylim([96 104]);
-save_fig(fig2,[fp_out 'ORACLES2017_ames_roof_cal_c0_relative_zoom'],asktosave);
+save_fig(fig2,[fp_out fignames '_cal_c0_relative_zoom'],asktosave);
 
 fig3 = figure;
 ax1 = subplot(3,1,1);
 plot(w_vis,visc0_avg,'.-k'); hold on;
 plot(w_nir,nirc0_avg.*10.0,'.-k');hold off;
 ylabel('c0 (NIR c0 x10)'); ylim([0,800])
-title('4STAR C0 saved for good averages from ORACLES 2017')
+title(['4STAR C0 saved for good averages from ' fignames])
 ax2 = subplot(3,1,2);
 
 plot(w_vis,visc0_std./visc0_avg.*100.0,'.-k'); hold on;
@@ -166,7 +169,7 @@ ylabel('Relative std of c0 [%]');ylim([0,1]); grid;
 
 linkaxes([ax1,ax2,ax3],'x');
 xlabel('Wavelength [nm]'); 
-save_fig(fig3,[fp_out 'Oracles2017_ames_roof_cal_c0_avg_' gdaystr],asktosave);
+save_fig(fig3,[fp_out fignames '_cal_c0_avg_' gdaystr],asktosave);
 
 % Now save the new averaged c0 for use
 days_cell = '';
@@ -175,8 +178,8 @@ for i=1:length(i_avg);
     days_cell = [days_cell ', ' c(1:4) '-' c(5:6) '-' c(7:8)];
 end;
 %filesuffix=['refined_Langley_averaged_with_MLO_Nov17_airborne_Langley_and_highalt_AOD_on_' gdaystr '_ORACLES'];
-additionalnotes={'Langley at MLO and airborne Data outside 2.0x the STD of 501 nm Langley residuals were screened out. ';...
-                 'Including high Altitude rate spectra, matched to splined stratospheric AOD measured at MLO.';...
+additionalnotes={'Langley at MLO outside of 1.8x the STD of 501 nm Langley residuals were screened out. ';...
+                 'c0 from best days at MLO';...
                  ['Averages of c0 from multiple days including: ' days_cell '. See the list of files below']};
 daystr=label_daystr; %leg{i_avg(end)}(end-7:end);
 visfilename=fullfile(starpaths, [daystr '_VIS_C0_' filesuffix '.dat']);
