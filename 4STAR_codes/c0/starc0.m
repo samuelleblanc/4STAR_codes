4f7ab1f94a4aabc11e06a35ac4ef77b9a0e2b374
segalm75 <segalm75@users.noreply.github.com>
2018-09-20 10:21:31 -0700

    modifying starc0 pre-fix to include 4STAR but be compatible for the starc0 code

--- 4STAR_codes/c0/starc0.m
+++ 4STAR_codes/c0/starc0.m
@@ -53,7 +53,7 @@ switch instrumentname;
         if isnumeric(t); % time of the measurement is given; return the C0 of the time.
             if t>=datenum([2018 8 1 0 0 0]); %for ORACLES 2018
                  daystr = '20180811';
-                 filesuffix = 'refined_Langley_averaged_with_MLO_2018_Aug_11_12';
+                 filesuffix = '4STAR_refined_Langley_averaged_with_MLO_2018_Aug_11_12';
             elseif t>=datenum([2018 1 1 0 0 0]); %for COSR 2018 and on
                  daystr = '20180209';
                  %filesuffix = 'refined_averaged_MLO_inflight_polyfit_v2';
