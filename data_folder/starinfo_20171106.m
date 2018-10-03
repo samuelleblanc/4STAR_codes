9cb243a1048713864dd9b9a3f3ffb6ca9fe91688
kpistone <kristina.pistone@nasa.gov>
2018-10-03 16:26:13 -0700

    Fixing the presence/lack of Langleys on Nov 06 and 07 for the rooftop tests

--- data_folder/starinfo_20171106.m
+++ data_folder/starinfo_20171106.m
@@ -13,7 +13,6 @@ end
 
  
 s.ground=[datenum(2017,11,6,19,01,38) datenum(2017,11,7,0,14,27)]; 
-s.langley1 = [datenum(2017,11,06,14,50,00) datenum(2017,11,06,17,30,00)]; %approximately air mass 12-2 during this period...
  s.sd_aero_crit=0.01;  %Connor asked to add this line on 8/2/2017
 % Ozone and other gases 
 s.O3h=21; %  
--- data_folder/starinfo_20171107.m
+++ data_folder/starinfo_20171107.m
@@ -13,7 +13,7 @@ end
 
  
 s.ground=[datenum(2017,11,7,15,11,35) datenum(2017,11,8,0,26,24)]; 
-% s.langley1 = [datenum(2017,8,23,11,00,09) datenum(2017,8,23,12,24,52)];
+s.langley1 = [datenum(2017,11,07,14,50,00) datenum(2017,11,07,17,30,00)]; %approximately air mass 12-2 during this period...
  s.sd_aero_crit=0.01;  %Connor asked to add this line on 8/2/2017
 % Ozone and other gases 
 s.O3h=21; %  
