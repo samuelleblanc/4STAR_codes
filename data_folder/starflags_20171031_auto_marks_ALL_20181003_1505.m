fe8a743e1e0fa20fcf6e15561265b05b13e9d1ae
kpistone <kristina.pistone@nasa.gov>
2018-10-11 10:43:00 +0200

    auto-created flags for the rooftop 2017 measurements
    (I have no opinions on whether these auto-flags should be retained or
    not. I'm just using the files for langleys)

---
+++ data_folder/starflags_20171031_auto_marks_ALL_20181003_1505.m
@@ -0,0 +1,80 @@
+function marks = starflags_20171031_auto_marks_ALL_20181003_1505  
+ % starflags file for 20171031 created by auto on 20181003_1505 to mark ALL conditions 
+ version_set('20181003_1505'); 
+ daystr = '20171031';  
+ % tag = 10: unspecified_clouds 
+ % tag = 700: bad_aod 
+ marks=[ 
+ datenum('15:18:03') datenum('15:18:10') 700 
+datenum('15:21:55') datenum('15:22:01') 700 
+datenum('15:36:57') datenum('15:37:01') 700 
+datenum('15:55:47') datenum('15:55:52') 700 
+datenum('16:14:38') datenum('16:14:42') 700 
+datenum('16:33:28') datenum('16:33:33') 700 
+datenum('16:52:25') datenum('16:52:30') 700 
+datenum('17:11:18') datenum('17:11:23') 700 
+datenum('17:30:11') datenum('17:30:15') 700 
+datenum('17:49:07') datenum('17:49:11') 700 
+datenum('18:08:06') datenum('18:08:10') 700 
+datenum('18:27:04') datenum('18:27:09') 700 
+datenum('18:46:05') datenum('18:46:10') 700 
+datenum('19:05:02') datenum('19:05:07') 700 
+datenum('19:24:06') datenum('19:24:11') 700 
+datenum('19:43:05') datenum('19:43:10') 700 
+datenum('20:02:02') datenum('20:02:07') 700 
+datenum('20:21:01') datenum('20:21:05') 700 
+datenum('20:39:56') datenum('20:40:01') 700 
+datenum('20:58:55') datenum('20:58:59') 700 
+datenum('21:17:52') datenum('21:17:57') 700 
+datenum('21:36:52') datenum('21:36:56') 700 
+datenum('21:55:52') datenum('21:55:56') 700 
+datenum('22:14:42') datenum('22:14:47') 700 
+datenum('22:33:33') datenum('22:33:37') 700 
+datenum('22:52:24') datenum('22:52:28') 700 
+datenum('23:11:18') datenum('23:11:22') 700 
+datenum('23:30:10') datenum('23:30:15') 700 
+datenum('23:49:13') datenum('23:49:18') 700 
+datenum('-720:08:07') datenum('-720:08:11') 700 
+datenum('-720:19:33') datenum('-720:25:20') 700 
+datenum('-720:28:00') datenum('-720:28:04') 700 
+datenum('-720:32:12') datenum('-720:32:34') 700 
+datenum('15:21:49') datenum('15:22:08') 10 
+datenum('16:39:08') datenum('16:39:46') 10 
+datenum('16:39:49') datenum('16:40:05') 10 
+datenum('16:40:20') datenum('16:40:35') 10 
+datenum('16:40:39') datenum('16:41:05') 10 
+datenum('16:41:20') datenum('16:41:20') 10 
+datenum('16:42:26') datenum('16:42:28') 10 
+datenum('16:42:39') datenum('16:42:39') 10 
+datenum('16:43:39') datenum('16:43:43') 10 
+datenum('16:58:03') datenum('16:58:35') 10 
+datenum('16:58:53') datenum('16:58:54') 10 
+datenum('16:59:05') datenum('16:59:32') 10 
+datenum('16:59:50') datenum('16:59:57') 10 
+datenum('17:00:00') datenum('17:00:04') 10 
+datenum('17:00:22') datenum('17:00:24') 10 
+datenum('17:31:41') datenum('17:31:52') 10 
+datenum('17:31:56') datenum('17:32:00') 10 
+datenum('17:32:33') datenum('17:33:55') 10 
+datenum('17:33:58') datenum('17:34:03') 10 
+datenum('17:34:26') datenum('17:34:27') 10 
+datenum('17:35:22') datenum('17:35:22') 10 
+datenum('17:35:30') datenum('17:35:36') 10 
+datenum('17:42:12') datenum('17:42:16') 10 
+datenum('17:42:32') datenum('17:42:35') 10 
+datenum('17:42:43') datenum('17:42:50') 10 
+datenum('17:42:59') datenum('17:43:01') 10 
+datenum('17:43:16') datenum('17:43:16') 10 
+datenum('17:43:28') datenum('17:43:28') 10 
+datenum('17:43:39') datenum('17:43:49') 10 
+datenum('17:43:55') datenum('17:44:25') 10 
+datenum('17:44:34') datenum('17:44:34') 10 
+datenum('17:44:45') datenum('17:44:49') 10 
+datenum('17:47:40') datenum('17:47:44') 10 
+datenum('17:47:53') datenum('17:47:56') 10 
+datenum('17:48:03') datenum('17:48:09') 10 
+datenum('17:48:23') datenum('17:48:25') 10 
+datenum('-720:32:08') datenum('-720:32:13') 10 
+];  
+marks(:,1:2)=marks(:,1:2)-datenum('00:00:00')+datenum([daystr(1:4) '-' daystr(5:6) '-' daystr(7:8)]); 
+return
