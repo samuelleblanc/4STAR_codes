4f789c9f059f404342a4eba72f886b622a38b2a9
ConnorJFlynn <Connor.Flynn@pnnl.gov>
2018-11-20 14:57:15 -0800

    Updates related to starpath conventions

--- 4STAR_codes/star_utils/set_starpaths.m
+++ 4STAR_codes/star_utils/set_starpaths.m
@@ -11,19 +11,21 @@ function data = set_starpaths(dataset,reset)
 %   None, to run this code.  
 
 % Named directory paths:
-%   'stardat' where you want to find raw data files
-%   'starmat' where you want to find/put allstar.mat files
-%   'starsun' where you want to find/put starsun.mat files
-%   'starsky' where you want to find/put starsky.mat files
-%   'starfig'    where you want to find/put *.fig, *.png, etc.
-%   'starppt'     where you want to find/put *.ppt files
-
+%   'stardat'  where you want to find raw data files
+%   'starmat'  where you want to find/put allstar.mat files
+%   'starsun'  where you want to find/put starsun.mat files
+%   'starsky'  where you want to find/put starsky.mat files
+%   'starimg'  where you want to find/put *.png files
+%   'starfig'  where you want to find/put *.fig files
+%   'starppt'  where you want to find/put *.ppt files
 
 % Connor, 2017/12/05
 % Connor, 2018-08-12: Modified to accept a named 'dataset' and to return a 
 % struct containing the paths
+% Connor, 2018-11-20: Modified to define consistent path names
+% Fixed logic that was causing pre-defined paths to overwrite new paths
 %---------------------------------------------------------------------
-version_set('2.0');
+version_set('3.0');
 
 % get the version of matlab
 vv = version('-release');
@@ -60,23 +62,26 @@ if isafile([pathdir,dataset,'.mat'])&&~reset
 end
 if ~isavar('data')||isempty(data)||reset
    data.stardat =[];
-   data.starmat =[];
-   data.starsun =[];
-   data.starsky =[];
    data.starfig = [];
+   data.starimg = [];
+   data.starmat =[];
    data.starppt =[];
+   data.starsky =[];
+   data.starsun =[];
 end
  
 fields = fieldnames(data);
 for fld = 1:length(fields)
    field = fields{fld};
-   while ~isdir(data.(field)) || ~strcmp(data.(field),getnamedpath(field))
+   while ~isdir(data.(field)) 
+%    while ~isdir(data.(field)) || ~strcmp(data.(field),getnamedpath(field))
       if isempty(data.(field)) || ~isdir(getnamedpath(field))
          data.(field) = setnamedpath(field);
       else
          data.(field) = setnamedpath(field,getnamedpath(field));
       end
-   end
+   end   
+   setnamedpath(field,data.(field));
 end
 
 save([pathdir, 'lastdata.mat'],'-struct','data');
