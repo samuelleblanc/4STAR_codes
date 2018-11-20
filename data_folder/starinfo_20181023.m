58b803bfad0f397af028723f3189c72d16e37acb
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2018-11-19 18:57:15 -0800

    starflag updates and starinfo for ORACLES 10/17 and 10/23

--- data_folder/starinfo_20181023.m
+++ data_folder/starinfo_20181023.m
@@ -30,7 +30,7 @@ if isfield(s,'instrumentname')
                  
                  %s.AODuncert_constant_extra = 0.01;
     %             flags
-                 %s.flagfilename     = '20181019_starflag_man_created20181021_1532by_SL.mat'; 
+                 s.flagfilename     = '20181023_starflag_man_created20181119_1457by_SB.mat'; 
     %             s.flagfilenameCWV  = '20170831_starflag_CWV_man_created20170903_0109by_MS.mat';
     %             s.flagfilenameO3   = '20170831_starflag_O3_man_created20170903_0136by_MS.mat';
     %             s.flagfilenameNO2  = '20170831_starflag_NO2_man_created20170903_0147by_MS.mat';
