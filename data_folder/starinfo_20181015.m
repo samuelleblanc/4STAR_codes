8a7fb0d6baf1efb09e9da3dd6227f3c96330d75b
Samuel LeBlanc <samuelleblanc@users.noreply.github.com>
2019-04-30 14:45:41 -0700

    high alt c0 creation modifications

--- data_folder/starinfo_20181015.m
+++ data_folder/starinfo_20181015.m
@@ -15,6 +15,7 @@ end
 % s.langley1 = [datenum(2018,10,10,6,58,00) datenum(2018,10,10,7,8,00)]; %put in if condition below if different for different 4STARs
 %            s.flight=[datenum(2017,8,31,07,59,14) datenum(2017,8,31,15,52,10)]; 
 s.sd_aero_crit=0.01;  
+s.high_alt_c0 = [datenum(2018,10,15,10,00,00) datenum(2018,10,15,10,02,00)];
              
 % Ozone and other gases 
 s.O3h=21; %  
