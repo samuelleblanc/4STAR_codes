430e4c73ad1885e4e8e903b97f2a508372026b6b
ConnorJFlynn <conjo.flynn@gmail.com>
2020-05-06 23:34:35 -0500

    Update alm_sym_test.m
    Modified to select judicious subset of input WL. This change was necessary when producing GRASP output otherwise the symmetry test gets swamped by so many WLs, some with weak radiance signals.

--- 4STAR_codes/skyscan_utils/alm_sym_test.m
+++ 4STAR_codes/skyscan_utils/alm_sym_test.m
@@ -7,19 +7,21 @@ good_almA = star.good_almA & ~any(isNaN(star.skyrad(:,star.aeronetcols)),2);
 good_almB = star.good_almB & ~any(isNaN(star.skyrad(:,star.aeronetcols)),2);
 almA_ii = find(good_almA);
 almB_ii = find(good_almB);
+wl_test = [440,615,870]; 
+wl_test_ii = interp1(1000.*star.w(star.aeronetcols),1:length(star.aeronetcols),wl_test,'nearest');
 if star.isALM && ~isempty(almA_ii)&&~isempty(almB_ii)
     [inA, inB] = nearest(star.SA(almA_ii), star.SA(almB_ii));
     Az_A = mod(star.Az_sky(almA_ii(inA))-star.sunaz((almA_ii(inA))),180);
     Az_B = mod(star.Az_sky(almB_ii(inB))-star.sunaz((almB_ii(inB))),180);
     az_180 = Az_A>175 & Az_B>175;
     SA_A = star.SA(almA_ii(inA)); SA_B = star.SA(almB_ii(inB));
-    rad_A = star.skyrad(almA_ii(inA),star.aeronetcols);
-    rad_B = star.skyrad(almB_ii(inB),star.aeronetcols);
+    rad_A = star.skyrad(almA_ii(inA),star.aeronetcols(wl_test_ii));
+    rad_B = star.skyrad(almB_ii(inB),star.aeronetcols(wl_test_ii));
     rad_sym = abs((rad_A - rad_B)./((rad_A + rad_B)./2));
     
     % AERONET requires <20% except for Az=180 which is <5%
-    sym = ~any(rad_sym(:,1:end-1)>0.20,2);
-    sym(az_180) = ~any(rad_sym(az_180,1:end-1)>0.05,2);
+    sym = ~any(rad_sym>0.20,2);
+    sym(az_180) = ~any(rad_sym(az_180,:)>0.05,2);
     good_almA(almA_ii(inA(~sym))) = false;
     good_almB(almB_ii(inB(~sym))) = false;
     
@@ -29,7 +31,7 @@ if star.isALM && ~isempty(almA_ii)&&~isempty(almB_ii)
     plot(SA_A, rad_A,'-o', SA_B, rad_B,'-+',SA_A + bad, rad_A,'rx');
     title('Alm symmetry test')
     ss(2)=subplot(3,1,2);
-    plot(SA_A,100.*rad_sym(:,1:end-1),'-o',SA_A+bad,100.*rad_sym(:,1:end-1),'rx');
+    plot(SA_A,100.*rad_sym,'-o',SA_A+bad,100.*rad_sym,'rx');
     ylabel('% diff'); xlabel('scattering angle [deg]');
     ss(3)=subplot(3,1,3);
     plot(SA_A,SA_A-SA_B,'-rx');
