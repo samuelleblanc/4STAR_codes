4851c5997c69a45d5d73fd3d4b43d58cf7e7f308
ConnorJFlynn <conjo.flynn@gmail.com>
2021-03-25 15:07:10 -0500

    Reviewing aod basis fitting
    Reviewing normalized and non-normalized basis sets and fitting with log-aod vs aod.

--- 4STAR_codes/star_utils/fit_aod_basis_.m
+++ 4STAR_codes/star_utils/fit_aod_basis_.m
@@ -1,5 +1,5 @@
-function [aod_fit, fit_rms, Ks,log_modes] = fit_aod_basis(wl, aod, wl_out);
-% aod_fit = fit_aod_basis(wl, aod,wl_out);
+function [aod_fit, fit_rms, Ks,log_modes] = fit_aod_basis_(wl, aod, wl_out);
+% aod_fit = fit_aod_basis_(wl, aod,wl_out);
 % wl should be in nm and Mx1 or 1xM
 % aod should be Mx1 or 1xM or MxN with M matchin wl
 % wl_out should be Nx1 or 1xN
@@ -9,6 +9,10 @@ function [aod_fit, fit_rms, Ks,log_modes] = fit_aod_basis(wl, aod, wl_out);
 
 % 2020-11-26: v1.0, Connor added version control
 % 2021-02-19: v1.1, Connor, using normalized basis in  added version control
+% 2021-02-19: v1.2, Connor, testing with both log_mode and "non-log" modes
+%                   Nearly equivalent RMS for log and non-log fitting but
+%                   at least for aeronet&mfrsr tests log-fit appears to
+%                   extrapolate better (lower curvature at both ends)
 version_set('1.1');
 % TBD: augment to output Ks, Cn, PDF etc.  Important if we want to use this
 % to identify "dust" for example, or identify/remove "425 bip"
@@ -41,31 +45,49 @@ if size(wl,2)==1 && all(size(wl)==size(aod))
     sub_modes = aod_mode.log_nmodes;
 %     sub_modes = sub_modes -(ones(size(sub_modes,1),1)*max(sub_modes));
     log_modes = interp1(aod_mode.log_wl, sub_modes,log(wl),'linear','extrap');
+    nonlog_modes = interp1(aod_mode.log_wl, exp(sub_modes),log(wl),'linear','extrap');
     
+% Test both log_modes and nonlog_modes    
     done = false; 
     good = true(1,size(log_modes,2));
     good_ii = find(good);
-    
+    done_ = false; 
+    good_ = true(1,size(nonlog_modes,2));
+    good_ii_ = find(good_);
     %The idea here is to eliminate unnecessary components by iteratively 
     % throwing out the least significant one until all exceed 1e-6.
     % Initially I normalized by the max coef, but the exponential blew this
     % up in certain cases, pushing values toward inf or -inf.  The
     % alternate approach below avoids this by sorting in log-space and only
     % assessing the smallest value against the threshold
-
-    while ~done && sum(good)>1
-        Ks(good) = real(log(aod))'/real(log_modes(:,good))';
-%         Ks = fit_it_2(log(aod), log_modes(:,good_ii));
-         nKs(good) = Ks(good)-max(Ks(good));      
-        [minKs,min_ii] = min(nKs(good_ii));
-        if exp(minKs)<1e-6
-            Ks(good_ii(min_ii)) = 0; nKs(good_ii(min_ii)) = 0;
-            good(good_ii(min_ii)) = false; 
-            good_ii = find(good);
-        else
-            done = true;
+    while (~done && sum(good)>1) || (~done_ && sum(good_)>1)
+        if ~done && sum(good)>1
+            Ks(good) = real(log(aod))'/real(log_modes(:,good))';
+            nKs(good) = Ks(good)-max(Ks(good));
+            [minKs,min_ii] = min(nKs(good_ii));
+            if exp(minKs)<1e-6
+                Ks(good_ii(min_ii)) = 0; nKs(good_ii(min_ii)) = 0;
+                good(good_ii(min_ii)) = false;
+                good_ii = find(good);
+            else
+                done = true;
+            end
+        end
+        if ~done_ && sum(good_)>1
+            Ks_(good_) = real(aod)'/real(nonlog_modes(:,good_))';
+            nKs_(good_) = Ks_(good_)./max(Ks_(good_));
+            [minKs_,min_ii_] = min(nKs_(good_ii_));
+            
+            if minKs_<0
+                Ks_(good_ii_(min_ii_)) = 0; nKs_(good_ii_(min_ii_)) = 0;
+                good_(good_ii_(min_ii_)) = false;
+                good_ii_ = find(good_);
+            else
+                done_ = true;
+            end
         end
     end
+    
     Ks(~good) = NaN; nKs(~good) = NaN;
     log_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl),'linear','extrap');    
     log_fit = log_modes*Ks(good)';
@@ -75,12 +97,20 @@ if size(wl,2)==1 && all(size(wl)==size(aod))
     log_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl_out),'linear','extrap');
     log_fit = log_modes*Ks(good)'; 
     aod_fit = exp(log_fit); 
-        
-%  figure_(2021); plot(wl, aod, 'r*',wl_out, aod_fit,'-ko'); logx; logy
+
+    Ks_(~good_) = NaN; nKs_(~good_) = NaN;
+    nonlog_modes = interp1(aod_mode.log_wl, exp(sub_modes(:,good_ii_)),log(wl),'linear','extrap');    
+    nonlog_fit = nonlog_modes*Ks_(good_)';   
+    fit_rms_ = sqrt(nanmean((aod-nonlog_fit).^2));
+    
+    nonlog_modes = interp1(aod_mode.log_wl, exp(sub_modes(:,good_ii_)),log(wl_out),'linear','extrap');
+    nonlog_fit = nonlog_modes*Ks_(good_)'; 
+    
+    figure_(2021); plot(wl, aod, 'r*',wl_out, aod_fit,'-ko', wl_out, nonlog_fit,'-b+'); logx; logy
+    legend('meas',sprintf('log-fit RMS=%1.2e',fit_rms), sprintf('non-log RMS=%1.2e',fit_rms_));
     if any(wl_out_size ~= size(wl_out))
         aod_fit = aod_fit';
-    end
-    
+    end    
 else
     warning('wl and aod must be row vectors of same length')
     aod_fit = NaN(size(aod)); Ks = []; log_modes = [];
--- 4STAR_codes/star_utils/fit_aod_nbasis.m
+++ 4STAR_codes/star_utils/fit_aod_nbasis.m
@@ -1,17 +1,15 @@
-function [aod_fit, fit_rms, Ks,log_modes] = fit_aod_basis(wl, aod, wl_out);
-% aod_fit = fit_aod_basis(wl, aod,wl_out);
+function [aod_fit, fit_rms, Ks,log_nmodes] = fit_aod_nbasis(wl, aod, wl_out);
+% [aod_fit, fit_rms, Ks,log_nmodes] = fit_aod_nbasis(wl, aod,wl_out);
 % wl should be in nm and Mx1 or 1xM
 % aod should be Mx1 or 1xM or MxN with M matchin wl
 % wl_out should be Nx1 or 1xN
-% Fits supplied AOD as a combination of log_aod basis vectors at selected
+% Fits log of supplied AOD as a combination of log_aod normalized basis vectors at selected
 % wavelengths and returns over extended
 % Testing with new basis from observed anet PDFs in vmr and std
 
 % 2020-11-26: v1.0, Connor added version control
-% 2021-02-19: v1.1, Connor, using normalized basis in  added version control
+% 2020-11-26: v1.1, Connor, testing with basis normalized to unity
 version_set('1.1');
-% TBD: augment to output Ks, Cn, PDF etc.  Important if we want to use this
-% to identify "dust" for example, or identify/remove "425 bip"
 
 if ~any(wl)>100
     wl = wl*1000;
@@ -37,9 +35,16 @@ if size(wl,2)==1 && all(size(wl)==size(aod))
     if ~any(wl_out)>100
         wl_out = wl_out * 1000;
     end
-    aod_mode = load([strrep(which('fit_aod_basis.m'),'fit_aod_basis.m','aod_SD_nmode.mat')]);   
+    aod_mode = load([strrep(which('fit_aod_nbasis.m'),'fit_aod_nbasis.m','aod_SD_nmode.mat')]);
+    
+    % Seems to work OK for MFRSR + anetmode.
     sub_modes = aod_mode.log_nmodes;
-%     sub_modes = sub_modes -(ones(size(sub_modes,1),1)*max(sub_modes));
+%     sub_modes = sub_modes./(ones(size(sub_modes,1),1)*max(sub_modes));
+%     
+%     sub_modes = aod_mode.log_modes;
+%     sub_modes = sub_modes./(ones(size(sub_modes,1),1)*max(sub_modes));
+    % sub_i = [2:2:length(aod_mode.vmr)];
+    % sub_modes = sub_modes(:,sub_i);
     log_modes = interp1(aod_mode.log_wl, sub_modes,log(wl),'linear','extrap');
     
     done = false; 
@@ -52,7 +57,8 @@ if size(wl,2)==1 && all(size(wl)==size(aod))
     % up in certain cases, pushing values toward inf or -inf.  The
     % alternate approach below avoids this by sorting in log-space and only
     % assessing the smallest value against the threshold
-
+    % Perhaps a better approach would be to exclude coefficients based on 
+    % contributing less than x% of the total at all wavelengths.
     while ~done && sum(good)>1
         Ks(good) = real(log(aod))'/real(log_modes(:,good))';
 %         Ks = fit_it_2(log(aod), log_modes(:,good_ii));
@@ -66,21 +72,30 @@ if size(wl,2)==1 && all(size(wl)==size(aod))
             done = true;
         end
     end
-    Ks(~good) = NaN; nKs(~good) = NaN;
-    log_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl),'linear','extrap');    
-    log_fit = log_modes*Ks(good)';
-    aod_fit = exp(log_fit); 
-    fit_rms = sqrt(nanmean((aod-aod_fit).^2));
     
-    log_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl_out),'linear','extrap');
-    log_fit = log_modes*Ks(good)'; 
-    aod_fit = exp(log_fit); 
-        
-%  figure_(2021); plot(wl, aod, 'r*',wl_out, aod_fit,'-ko'); logx; logy
+    slog_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl_out),'linear','extrap');
+    log_modes = interp1(aod_mode.log_wl, sub_modes(:,good_ii),log(wl),'linear','extrap');
+    log_aod_fit = slog_modes*Ks(good)'; log_aod = log_modes*Ks(good)';
+    aod_fit = exp(log_aod_fit); aod_fit_ = exp(log_aod); fit_rms = sqrt(nanmean((aod-aod_fit_).^2));
+%         figure_(2020); plot(wl, aod, 'r*',wl_out, aod_fit,'-kx'); logx; logy
     if any(wl_out_size ~= size(wl_out))
         aod_fit = aod_fit';
     end
     
+    %  figure_(2020); plot(wl, aod, 'r*',wl_out, aod_fit,'-kx'); logx; logy
+    
+    % bins = unique(aod_mode.bin_radius(:));
+    % md_PSD = 0.*ones(length(Ks),length(bins));
+    % for k = 1:length(Ks)
+    %     md_PSD(k,:) = interp1(aod_mode.bin_radius(k,:),exp(Ks(k)).*aod_mode.PSD(k,:), bins);
+    % end
+    % md_PSD(isnan(md_PSD))= 0;
+    % PSD = sum(md_PSD); PSD(PSD==0) = NaN;
+    % figure; plot(1000.*bins, PSD,'-o');logx; logy
+    % xlabel('bin radius [um]');
+    
+    % figure; loglog(wl, aod, 'o-',wl_out, aod_fit,'k-');
+    % figure; plot(exp(aod_mode.log_wl), exp(aod_mode.log_modes), '-'); logy; logx
 else
     warning('wl and aod must be row vectors of same length')
     aod_fit = NaN(size(aod)); Ks = []; log_modes = [];
