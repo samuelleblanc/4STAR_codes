69b2626a590ce15f68e50b1ee5d7e8cf046cdbf7
ConnorJFlynn <conjo.flynn@gmail.com>
2021-04-09 23:48:57 -0500

    Final commit including gitignore

--- 4STAR_codes/update_toggle.m
+++ 4STAR_codes/update_toggle.m
@@ -15,10 +15,10 @@ toggle_out.savefigure=false;
 toggle_out.computeerror=true;
 toggle_out.inspectresults=false;
 toggle_out.applynonlinearcorr=true;
-toggle_out.applytempcorr=true;% true is for SEAC4RS data
+toggle_out.applytempcorr=false;% true is for SEAC4RS data
 toggle_out.gassubtract = true;
 toggle_out.booleanflagging = false; % for running any Yohei style flagging
-toggle_out.starflag_mode = 2; % for starflag, mode=1 for automatic, mode=2 for in-depth 'manual'
+toggle_out.starflag_mode = 1; % for starflag, mode=1 for automatic, mode=2 for in-depth 'manual'
 toggle_out.flagging = toggle_out.starflag_mode; % Defunct but kept for old codes
 toggle_out.doflagging = false; % for running any Yohei style flagging
 toggle_out.dostarflag = true; % for newer style flagging
@@ -32,13 +32,13 @@ toggle_out.save_marks_flags = false;
 toggle_out.no_SSFR = false;
 toggle_out.use_last_wl = true;
 toggle_out.custom_polyfit = true;
-toggle_out.flip_toggle = false;
+toggle_out.flip_toggle = true;
 toggle_out.skip_ppl = false;
 toggle_out.skip_alm = false;
-toggle_out.skip_lt_N = 0;
+toggle_out.skip_lt_N = 1;
 toggle_out.skip_gt_N = inf;
-toggle_out.sky_tag = {''}; %'_minusdAOD'; %_minusdAOD'; % descriptive tag applied to input filename
-toggle_out.skyscan_manual = false; % true for skyscan manual selection, i.e. manual interaction and to set defaults
+toggle_out.sky_tag = {'_5wl_360_xfit_more_complete'}; %'_minusdAOD'; % descriptive tag applied to input filename, leading underscore pls
+toggle_out.skyscan_manual = true; % true for skyscan manual selection, i.e. manual interaction and to set defaults
 toggle_out.sky_SA_min = 3.5;
 toggle_out.sky_El_min = 6;
 toggle_out.sky_dOD =0; % -[0.055 0.036 0.029 0.024 0.037];%<-0924 after11a %[0.06 0.042 0.035 0.029 0.037];%<-0924 before11a%[0.024 0.018 0.016 0.013 0.025];%<-0925 %[0.011 0.009 0.007 0.006 0.011];%<-0927 (5pm) %[0.024 0.02 0.018 0.018 0.025]; %<-0831a[not scans 9&10] %[0.022 0.018 0.017 0.016 0.02];%<-0902 %[0.026 0.015 0.009 0.008 0.018]; %<-0904 %[0.028 0.017 0.01 0.01 0.019];%<-0906 %[0.025 0.021 0.022 0.025 0.033];%<- 0918 %[0.018 0.015 0.01 0.01 0.018]; %<-0914 %[0.028 0.019 0.013 0.013 0.022]; %<-0910 %[0.024 0.016 0.01 0.01 0.02]; %<-0908  %0;%-[0.018 0.013 0.01 0.01 0.02]; %<- 0912 %[0]; %-[0.036 0.03 0.03 0.03 0.036]; <-0920 % tod = tod + dOD %
