4eb3e1eff8ca44809329404488b16147124e2b0d
ConnorJFlynn <conjo.flynn@gmail.com>
2023-11-28 12:45:20 -0600

    Update anet_alm_SA_and_radiance.m

--- 4STAR_codes/anet_alm_SA_and_radiance.m
+++ 4STAR_codes/anet_alm_SA_and_radiance.m
@@ -3,7 +3,7 @@ function alm = anet_alm_SA_and_radiance(infile)
 % Pack data from aeronet alm file into structure with radiance dimensioned
 % against time and azimuth angle, and compute scattering angles.
 if ~isavar('infile')||~isafile(infile)
-    infile = getfullname('*.alm','aeronet_sky','Select AERONET AIP ALM file.');
+    infile = getfullname(['*.alm','*.csv'],'aeronet_sky','Select AERONET AIP ALM file.');
 end
 alm = rd_anetaip_v3(infile);
 fld = fieldnames(alm);
