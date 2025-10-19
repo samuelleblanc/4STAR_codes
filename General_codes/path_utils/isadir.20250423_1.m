557836f0f8d5022ca75e7f03b20ad83f0943630e
Connor J Flynn <conjo.flynn@gmail.com>
2025-10-19 15:20:46 -0500

    Create isadir.20250423_1.m

---
+++ General_codes/path_utils/isadir.20250423_1.m
@@ -0,0 +1,28 @@
+function result = isadir(dirpath)
+
+%ISADIR replaces "isdir" and "isfolder" for 2013b and earlier.
+% Returns true or false if dirpath contains strings representing valid
+% directories or folders.  Arguement dirpath may be a char or cell array of
+% chars.
+%
+%ISADIR  True if argument is a directory.
+%   ISADIR(DIR) returns a 1 if DIR is a directory and 0 otherwise.
+%
+%   See also FINFO, MKDIR.
+%   C. Flynn 2018-11-26
+
+
+result = false;
+if ~isempty(dirpath)
+    if iscell(dirpath)
+        if length(dirpath)==1
+            result = isadir(dirpath{1});
+        else
+            result = [isadir(dirpath{1}), isadir(dirpath(2:end))];
+        end
+    else
+        result = isempty(findstr(dirpath,'*'))&&isempty(findstr(dirpath,'?'))&&length(dir(dirpath))>=2;
+    end
+end
+    % result = exist(dirpath,'dir') == 7;
+return
